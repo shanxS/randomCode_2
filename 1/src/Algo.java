6c2046f91b5f18fefbbf255cbe3518d32bf1a5ab
shanxS <shanx.shashank@gmail.com>
2016-03-31 19:26:45 +0530

    finding number wiht odd occurance

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -2,28 +2,16 @@
  * @author shashaku on 29/03/16.
  */
 public class Algo {
-    private Integer[] A =  {0,0,1,2};//{1, 3, 3, 5, 2};//{2,2,2,1};
+    private Integer[] A =  {-1,-1,2,2,3,3};
 
 
     public void run() {
-        for (Integer i=0; i<A.length; ++i) {
-            Integer orig = A[i] % A.length;
-            Integer addend = (orig == 0) ? A.length : (A.length * (orig));
-            A[orig] += addend;
-        }
-
-        Integer maxValue = 0;
-        Integer maxCount = (A[0] - (A[0] % A.length)) / A.length;
+        Integer xor = A[0];
         for (Integer i=1; i<A.length; ++i) {
-            Integer orig = A[i] % A.length;
-            Integer count = (A[i] - orig) / (A.length * (i));
-
-            if (count > maxCount) {
-                maxCount = count;
-                maxValue = i;
-            }
+            xor ^= (A[i]);
         }
 
-        System.out.print(maxValue + " " + maxCount);
+            System.out.print("odd " + (xor));
+
     }
 }
