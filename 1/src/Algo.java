6bf7273cee375bf0c461b8026e5010622b448480
shanxS <shanx.shashank@gmail.com>
2016-04-02 20:25:13 +0530

    generic Maximum sum such that no two elements are adjacent

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -5,22 +5,29 @@ import java.util.*;
  */
 public class Algo {
 
-    private Integer[] A = {25,5,10,100,10,5};//{1, 2, 3, 6};
+    private Integer[] A = {25,20,100};//{25, 100, 10};//{25,5};//{25,5,10,100,10,5,50};//{1, 2, 3, 6};
     private HashMap<Integer, Integer> positionMax = new HashMap<>();
     private Integer globalMax = Integer.MIN_VALUE;
 
 
     public void run() {
-        positionMax.put(A.length-1, A[A.length-1]);
-        positionMax.put(A.length-2, A[A.length-2]);
-        positionMax.put(A.length-3, A[A.length-3] + A[A.length-1]);
 
-        for (Integer i=A.length-4; i>=0; --i) {
+        if (A.length == 2) {
+            System.out.print(Math.max(A[0], A[1]));
+        } else if (A.length == 3) {
+            System.out.print(Math.max((A[0] + A[2]), A[1]));
+        } else {
+            positionMax.put(0, A[0]);
+            positionMax.put(1, A[1]);
+            positionMax.put(2, A[2] + A[1]);
 
-            Integer thisMax = A[i] + Math.max(positionMax.get(i+2), positionMax.get(i+3));
-            positionMax.put(i, thisMax);
+            for (Integer i = 3; i < A.length; ++i) {
+
+                Integer thisMax = A[i] + Math.max(positionMax.get(i - 2), positionMax.get(i - 3));
+                positionMax.put(i, thisMax);
+            }
+
+            System.out.print(Math.max(positionMax.get(A.length-1), positionMax.get(A.length-2)));
         }
-
-        System.out.print(positionMax);
     }
 }
