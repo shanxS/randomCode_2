26c4982c0fc81c609ffc779816d64bb0ab3d0edd
shanxS <shanx.shashank@gmail.com>
2016-04-02 20:18:02 +0530

    Maximum sum such that no two elements are adjacent

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -1,31 +1,26 @@
+import java.util.*;
+
 /**
  * @author shashaku on 29/03/16.
  */
 public class Algo {
 
-    private Integer[] A = {1,2,3,4, 5};//{1, 2, 3, 6};
-    private Integer turns = 2;
+    private Integer[] A = {25,5,10,100,10,5};//{1, 2, 3, 6};
+    private HashMap<Integer, Integer> positionMax = new HashMap<>();
+    private Integer globalMax = Integer.MIN_VALUE;
 
 
     public void run() {
-        reverse(0,turns-1);
-        reverse(turns, A.length-1);
-        reverse(0, A.length-1);
+        positionMax.put(A.length-1, A[A.length-1]);
+        positionMax.put(A.length-2, A[A.length-2]);
+        positionMax.put(A.length-3, A[A.length-3] + A[A.length-1]);
 
-        for (Integer ele : A) {
-            System.out.print(ele + " ");
+        for (Integer i=A.length-4; i>=0; --i) {
+
+            Integer thisMax = A[i] + Math.max(positionMax.get(i+2), positionMax.get(i+3));
+            positionMax.put(i, thisMax);
         }
+
+        System.out.print(positionMax);
     }
-
-    private void reverse(Integer start, Integer end) {
-        while (start < end) {
-            Integer temp = A[start];
-            A[start] = A[end];
-            A[end] = temp;
-
-            ++start;
-            --end;
-        }
-    }
-
 }
