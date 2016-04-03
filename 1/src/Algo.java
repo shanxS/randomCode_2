bd937ada6c2de0b7e8eaa898d4f9d743fe23909e
shanxS <shanx.shashank@gmail.com>
2016-04-03 15:20:26 +0530

    iversions

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -1,59 +1,87 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author shashaku on 29/03/16.
  */
 public class Algo {
 
     public void run() {
-        Integer[] A = {2, 5, 2, 8, 5, 6, 8, 8};
-        BST bst = new BST();
+        Integer[] A = {2, 4, 1, 3, 5};//{1, 20, 6, 4, 5};
+        InversionKeeper ik = new InversionKeeper();
         for (Integer ele : A) {
-            bst.add(ele);
+            ik.insert(ele);
         }
 
-        bst.print(bst.getHead());
+        ik.printInversions();
+
     }
 }
 
-
-class BST {
+class InversionKeeper {
     private Node head;
+    private Map<Integer, List<Integer>> inversions;
 
-    public void add(Integer data) {
+    public InversionKeeper() {
+        inversions = new HashMap<>();
+    }
+
+    public void insert(Integer data) {
         if (head == null) {
-            head = new Node (data);
+            head = new Node(data);
         } else {
             insert(head, data);
         }
     }
 
+    public void printInversions() {
+        for (Map.Entry<Integer, List<Integer>> entry : inversions.entrySet()) {
+            System.out.println(entry);
+        }
+    }
+
     private void insert(Node node, Integer data) {
         if (node.getData() > data) {
+            populateInversions(node, data);
             if (node.getLeft() != null) {
                 insert(node.getLeft(), data);
             } else {
                 node.setLeft(new Node(data));
             }
-        } else if (node.getData() < data) {
+        } else  if (node.getData() < data) {
             if (node.getRight() != null) {
                 insert(node.getRight(), data);
             } else {
                 node.setRight(new Node(data));
             }
-        } else if (node.getData() == data) {
-            node.incrementCount();
         }
     }
 
-    public Node getHead() { return head; }
+    private void populateInversions(Node node, Integer data) {
+        if (node == null) {
+            return;
+        }
+
+        List<Integer> inversionData = inversions.get(node.getData());
+        if (inversionData == null) {
+            inversionData = new ArrayList<>();
+            inversions.put(node.getData(), inversionData);
+        }
+        inversionData.add(data);
+
+        populateInversions(node.getRight(), data);
+    }
 
     public void print(Node node) {
         if (node == null) {
             return;
         }
 
-        System.out.print(node.getData() + "[" + node.getCount() + "]" + " - ");
+        System.out.print(node.getData() + " - ");
         if (node.getLeft() != null) {
-            System.out.print(node.getLeft().getData() + " ");
+            System.out.print(node.getLeft().getData());
         }
         System.out.print(", ");
         if (node.getRight() != null) {
@@ -67,12 +95,19 @@ class BST {
 }
 
 class Node {
+    private Integer data;
     private Node left, right;
-    private Integer data, count;
 
     public Node (Integer data) {
         this.data = data;
-        this.count = 1;
+    }
+
+    public Integer getData() {
+        return data;
+    }
+
+    public void setData(Integer data) {
+        this.data = data;
     }
 
     public Node getLeft() {
@@ -89,21 +124,5 @@ class Node {
 
     public void setRight(Node right) {
         this.right = right;
-    }
-
-    public Integer getData() {
-        return data;
-    }
-
-    public void setData(Integer data) {
-        this.data = data;
-    }
-
-    public void incrementCount() {
-        ++count;
-    }
-
-    public Integer getCount() {
-        return count;
     }
 }
