30762797eae9363a5171e7266c7594b052094c77
shanxS <shanx.shashank@gmail.com>
2016-04-04 19:14:42 +0530

    Find the smallest and second smallest element in an array

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -3,66 +3,23 @@
  */
 public class Algo {
 
-    private Integer[] A = {1, 60, -10, 70, -80, 85};
+    private Integer[] A = {12, 13, 1, 10, 34, 1};
 
     public void run() {
 
-        QuickSort qs = new QuickSort();
-        qs.sort(A);
+        Integer small = Integer.MAX_VALUE;
+        Integer small2 = Integer.MAX_VALUE;
 
-        Integer end = A.length-1;
-        Integer start = 0;
-        Integer minValue = Integer.MAX_VALUE;
-        while (end > start) {
-            Integer diff = A[end] + A[start];
-            minValue = (Math.abs(diff) < Math.abs(minValue)) ? diff : minValue;
-
-            if (diff > 0) {
-                --end;
-            } else  if (diff < 0) {
-                ++start;
-            }
-            else if (diff == 0) {
-                break;
+        for (Integer i=0; i<A.length; ++i) {
+            if (A[i] < small) {
+                small2 = small;
+                small = A[i];
+            } else if (A[i] < small2 && A[i] != small) {
+                small2 = A[i];
             }
         }
 
-        System.out.print(minValue);
+        System.out.print(small + " " + small2);
 
-    }
-}
-
-class QuickSort {
-    private Integer[] A;
-    public void  sort (Integer[] A) {
-        this.A = A;
-        doItFor(0, A.length-1);
-    }
-
-    private void doItFor(int start, int end) {
-        if (start >= end) {
-            return;
-        }
-
-        Integer marker = start - 1;
-        Integer pivotValue = A[end];
-
-        for (Integer j=start; j<end; ++j) {
-            if (A[j] <= pivotValue) {
-                ++marker;
-                swap(marker, j);
-            }
-        }
-
-        swap(marker+1, end);
-
-        doItFor(start, marker);
-        doItFor(marker+2, end);
-    }
-
-    private void swap(Integer from, Integer to) {
-        Integer temp = A[from];
-        A[from] = A[to];
-        A[to] = temp;
     }
 }
