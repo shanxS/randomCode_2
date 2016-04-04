d957843e0e570e84f35249ea7fa7e149cef60ada
shanxS <shanx.shashank@gmail.com>
2016-04-04 20:29:08 +0530

    first and lsat element in sorted array using binary search

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -6,44 +6,63 @@ public class Algo {
     private Integer[][] ARR = {{1, 1, 1, 2, 2},
                              {1, 1, 2, 4, 4, 4, 6, 6},
                              {1, 2, 3, 3, 3, 3, 10},
-                             {12, 13, 1, 10, 34, 1}};
+            {1, 2, 3, 3, 3, 3, 10}};
+    private Integer[] values = {1,4,3,10};
 
     public void run() {
-
-        for (Integer[] arr : ARR) {
-            find(arr);
+        for (Integer i=0; i<ARR.length; ++i) {
+            System.out.println("first " + firstIndex(values[i], ARR[i]));
+            System.out.println("last " + lastIndex(values[i], ARR[i]));
+            System.out.println();
         }
-
 
     }
 
-    private void find(Integer[] A) {
-        Integer num = A[0];
-        Integer count = 1;
+    private Integer lastIndex(Integer value, Integer[] A) {
+        Integer start = 0;
+        Integer end = A.length-1;
 
-        for (Integer i=1; i<A.length; ++i) {
-            if (A[i] == num) {
-                ++count;
-            } else {
-                --count;
-                if (count == 0) {
-                    num = A[i];
-                    count = 1;
+        while (start <= end) {
+            Integer mid = mean(start, end);
+            if (A[mid] == value) {
+                if(mid < A.length-1 && A[mid+1] == value) {
+                    start = mid + 1;
+                } else {
+                    return mid;
                 }
+            } else if (A[mid] < value) {
+                start = start+1;
+            } else if (A[mid] > value) {
+                end = mid-1;
             }
         }
 
-        count = 0;
-        for (Integer i=0; i<A.length; ++i) {
-            if (A[i] == num) {
-                ++count;
+        return null;
+    }
+
+    private Integer firstIndex(Integer value, Integer[] A) {
+        Integer start = 0;
+        Integer end = A.length-1;
+
+        while (start <= end) {
+            Integer mid = mean (start, end);
+            if (A[mid] == value) {
+                if (mid > 0 && A[mid-1] == value) {
+                    end = mid-1;
+                } else {
+                    return mid;
+                }
+            } else if (A[mid] < value) {
+                start = mid + 1;
+            } else if (A[mid] > value) {
+                end = mid - 1;
             }
         }
 
-        if (count >= A.length/2) {
-            System.out.println("foudn " + num);
-        } else {
-            System.out.println("not found");
-        }
+        return null;
+    }
+
+    private Integer mean(Integer num1, Integer num2) {
+        return Math.min(num1, num2) + ((Math.abs(num1-num2))/2);
     }
 }
