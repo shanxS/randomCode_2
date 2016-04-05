33d07527d311a88d93ce8234953727e2a3b83815
shanxS <shanx.shashank@gmail.com>
2016-04-05 17:45:09 +0530

    segregating 0s and 1s

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -3,35 +3,28 @@
  */
 public class Algo {
 
-    private final Integer MIN = 0;
-    private final Integer MAX = 1;
-    private Integer[] A = {1000, 11, 445, 1, 330, 3000};
+    private Integer[] A = {0, 1, 0, 1, 0, 0, 1, 1, 1, 0};
 
     public void run() {
 
-        Integer[] values = findMinMax(0, A.length-1);
-        System.out.print(values[MIN] + " " + values[MAX]);
-    }
 
-    private Integer[] findMinMax(Integer start, Integer end) {
-        Integer[] values = new Integer[2];
-
-        if (end-start == 0) {
-            values[MIN] = A[end];
-            values[MAX] = A[end];
-        } else if (end-start== 1) {
-            values[MIN] = Math.min(A[start], A[end]);
-            values[MAX] = Math.max(A[start], A[end]);
-        } else {
-            Integer mid = Math.min(start, end) + ((Math.abs(end-start))/2);
-            Integer[] values1 = findMinMax(start, mid);
-            Integer[] values2 = findMinMax(mid+1, end);
-
-            values[MIN] = Math.min(values1[MIN], values2[MIN]);
-            values[MAX] = Math.max(values1[MAX], values2[MAX]);
+        Integer marker = -1;
+        for (Integer i=0; i<A.length; ++i) {
+            if (A[i] == 0) {
+                ++marker;
+                swap(marker, i);
+            }
         }
 
-        return values;
+        for (Integer ele : A){
+            System.out.print(ele + " ");
+        }
+
     }
 
+    private void swap(Integer from, Integer to) {
+        Integer temp = A[from];
+        A[from] = A[to];
+        A[to] = temp;
+    }
 }
