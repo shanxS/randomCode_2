87060a7c1823ac9a5253022a048972023ede5620
shanxS <shanx.shashank@gmail.com>
2016-04-06 19:34:31 +0530

    Maximum difference between two elements such that larger element appears after the smaller number

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -2,103 +2,21 @@
  * @author shashaku on 29/03/16.
  */
 public class Algo {
-    private Integer[] A = {6, 5, 3, 1, 8, 7, 2, 4};
-    private Integer k = 4;//3;
+    private Integer[] A = {7, 9, 5, 6, 3, 2 };//{2, 3, 10, 6, 4, 8, 1};
+
     public void run() {
+        Integer maxDiff = Integer.MIN_VALUE;
+        Integer maxValue = A[A.length-1];
 
-        BST tree = new BST();
-        for (Integer ele : A) {
-            tree.add(ele);
-        }
-
-        tree.print();
-
-    }
-}
-
-class BST {
-    private Node head;
-
-    public void add (Integer value) {
-        if (head == null) {
-            head = new Node(value);
-        } else {
-            insert(head, value);
-        }
-    }
-
-    public void print() {
-        print(head);
-    }
-
-    private void print(Node node) {
-        if (node == null) {
-            return;
-        }
-
-        System.out.print(node.getData() + " - ");
-        if (node.getLeft() != null) {
-            System.out.print(node.getLeft().getData());
-        }
-        System.out.print(", ");
-        if (node.getRight() != null) {
-            System.out.print(node.getRight().getData());
-        }
-        System.out.println();
-
-        print(node.getLeft());
-        print(node.getRight());
-
-    }
-
-    private void insert(Node node, Integer value) {
-        if (node.getData() > value) {
-            if (node.getLeft() == null) {
-                node.setLeft(new Node(value));
-            } else {
-                insert(node.getLeft(), value);
+        for (Integer j=A.length-2; j>=0; --j) {
+            if (A[j] > maxValue) {
+                maxValue = A[j];
+            } else if (maxValue - A[j] > maxDiff) {
+                maxDiff = maxValue - A[j];
             }
-        } else if (node.getData() < value) {
-            if (node.getRight() == null) {
-                node.setRight(new Node(value));
-            } else {
-                insert(node.getRight(), value);
-            }
-        } else {
-            System.out.println("same values");
         }
-    }
-}
 
-class Node {
-    private Node left, right;
-    private Integer data;
+        System.out.print(maxDiff + " " + maxValue);
 
-    public Node(Integer data) {
-        this.data = data;
-    }
-
-    public Node getLeft() {
-        return left;
-    }
-
-    public void setLeft(Node left) {
-        this.left = left;
-    }
-
-    public Node getRight() {
-        return right;
-    }
-
-    public void setRight(Node right) {
-        this.right = right;
-    }
-
-    public Integer getData() {
-        return data;
-    }
-
-    public void setData(Integer data) {
-        this.data = data;
     }
 }
