ab958a4cf6b8b4412ee238e65f00f9dcf11a6f8d
shanxS <shanx.shashank@gmail.com>
2016-04-09 16:01:35 +0530

    kadane for min

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -3,44 +3,44 @@
  */
 public class Algo {
 
-//    public Integer[] A = {-2, -3, -4, -1, -2, -1, -5, -3};
-public Integer[] A = {2,-3,5};
+    public Integer[] A = {2, 3, -4, 1, 2, -1, -5, 3};
+    //public Integer[] A = {2,-3,5};
 
 
     public void run() {
-        Integer gMax = null;
+        Integer gMin = null;
         Integer gStart = null;
         Integer gEnd = null;
 
-        Integer lMax = null;
+        Integer lMin = null;
         Integer lStart = null;
         Integer lEnd = null;
 
         for (Integer i=0; i<A.length; ++i) {
-            if (lMax == null && A[i] > 0) {
-                lMax = A[i];
+            if (lMin == null && A[i] <= 0) {
+                lMin = A[i];
                 lStart = i;
                 lEnd = i;
-            } else if (lMax != null) {
-                lMax += A[i];
+            } else if (lMin != null) {
+                lMin += A[i];
                 lEnd = i;
 
-                if (lMax <= 0) {
-                    lMax = null;
+                if (lMin >= 0) {
+                    lMin = null;
                     lStart = null;
                     lEnd = null;
                 }
             }
 
-            if (lMax != null) {
-                if (gMax == null || lMax > gMax) {
-                    gMax = lMax;
+            if (lMin != null) {
+                if (gMin == null || lMin < gMin) {
+                    gMin = lMin;
                     gStart = lStart;
                     gEnd = lEnd;
                 }
             }
         }
 
-        System.out.print(gMax + " " + gStart + " " + gEnd);
+        System.out.print(gMin + " " + gStart + " " + gEnd);
     }
 }
