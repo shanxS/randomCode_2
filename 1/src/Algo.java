351f5c6d053e07c2cbf0d78a7d202c8974980bc6
shanxS <shanx.shashank@gmail.com>
2016-04-09 17:25:27 +0530

    Sort elements by frequency not stable

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -1,48 +1,53 @@
+import java.util.*;
+
 /**
  * @author shashaku on 29/03/16.
  */
 public class Algo {
 
-    private Integer[] A = {5,4,3,2,1};//{-7, -1, -5, -6, -3, -2};//{80, 2, 6, 3, 100};//{2, 3, 10, 6, 4, 8, 1};
+    private Integer[] A = {5,  2,  2,  8,  5,  6,  8,  8};
 
     public void run() {
-        Integer gMax = null;
-        Integer gStart = null;
-        Integer gEnd = null;
+        Map<Integer, Integer> keyFreq = new HashMap<>();
+        Map<Integer, List<Integer>> freqKeys = new LinkedHashMap<>();
 
-        Integer lMax = null;
-        Integer lStart = null;
-        Integer lEnd = null;
+        for (Integer i=0; i<A.length; ++i) {
+            Integer key = A[i];
 
-        for (Integer i=1; i<A.length; ++i) {
-            Integer value = A[i] - A[i-1];
-
-            if (lMax == null && value > 0) {
-                lMax = value;
-                lStart = i;
-                lEnd = i;
-            } else if (lMax != null) {
-                lMax += value;
-                lEnd = i;
-
-                if (lMax <= 0) {
-                    lMax = null;
-                    lStart = null;
-                    lEnd = null;
-                }
+            Integer freq = keyFreq.get(key);
+            if (freq == null) {
+                freq = 0;
             }
+            ++freq;
+            keyFreq.put(key, freq);
 
-            if (lMax != null) {
-                if (gMax == null || gMax < lMax) {
-                    gMax = lMax;
-                    gStart = lStart;
-                    gEnd = lEnd;
+            List<Integer> keys = freqKeys.get(freq);
+            if (keys == null) {
+                keys = new ArrayList<>();
+                freqKeys.put(freq, keys);
+            }
+            keys.add(key);
+
+            if (freq > 1) {
+                List<Integer> oldKeys = freqKeys.get(freq - 1);
+                oldKeys.remove(key);
+            }
+        }
+
+        for (Map.Entry<Integer, List<Integer>> entry : freqKeys.entrySet()) {
+            final Integer freq = entry.getKey();
+            List<Integer> keys = entry.getValue();
+
+            for (Integer key : keys) {
+                Integer counter = freq;
+                while (counter > 0) {
+                    System.out.print(key + " ");
+                    --counter;
                 }
             }
         }
 
-        System.out.print(gMax + " "
-                + ((gStart != null) ? (gStart-1) : ("null")) + " "
-                + ((gEnd != null) ? (gEnd) : ("null")));
+        System.out.println(freqKeys);
+        System.out.println(keyFreq);
     }
 }
