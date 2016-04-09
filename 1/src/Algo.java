669930a7e78cbb616c5a8afc1b3804042257d9fb
shanxS <shanx.shashank@gmail.com>
2016-04-09 17:42:13 +0530

    Sort elements by frequency stable

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -9,7 +9,7 @@ public class Algo {
 
     public void run() {
         Map<Integer, Integer> keyFreq = new HashMap<>();
-        Map<Integer, List<Integer>> freqKeys = new LinkedHashMap<>();
+        Map<Integer, List<Integer>> freqKeys = new TreeMap<>();
 
         for (Integer i=0; i<A.length; ++i) {
             Integer key = A[i];
@@ -20,17 +20,19 @@ public class Algo {
             }
             ++freq;
             keyFreq.put(key, freq);
+        }
+
+        for (Integer i=0; i<A.length; ++i) {
+            Integer key = A[i];
+            Integer freq = keyFreq.get(key);
 
             List<Integer> keys = freqKeys.get(freq);
             if (keys == null) {
                 keys = new ArrayList<>();
                 freqKeys.put(freq, keys);
             }
-            keys.add(key);
-
-            if (freq > 1) {
-                List<Integer> oldKeys = freqKeys.get(freq - 1);
-                oldKeys.remove(key);
+            if (!keys.contains(key)) {
+                keys.add(key);
             }
         }
 
