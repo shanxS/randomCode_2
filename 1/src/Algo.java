344d7613f230c5ae41e67990c5239f19d15ecf35
shanxS <shanx.shashank@gmail.com>
2016-04-10 14:57:45 +0530

    Find the Minimum length Unsorted Subarray, sorting which makes the complete array sorted

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -3,48 +3,56 @@
  */
 public class Algo {
 
-    private Integer[] A = {10, 12, 20, 30, 25, 40, 32, 31, 35, 50, 60};
-//    private Integer[] A = {0, 1, 15, 25, 6, 7, 30, 40, 50};
+//    private Integer[] A = {10, 12, 20, 30, 25, 40, 32, 31, 35, 50, 60};
+    private Integer[] A = {0, 1, 15, 25, 6, 7, 30, 40, 50};
 
     public void run() {
-        Integer maxSoFarBlipped = null;
-        Integer maxBlippingPoint = null;
-        Integer i=0;
-        while (i<A.length-1) {
-            if (A[i] > A[i + 1]) {
-                if (maxSoFarBlipped == null || A[maxSoFarBlipped] < A[i]) {
-                    maxSoFarBlipped = i;
 
-                    while (i<A.length-1 && A[maxSoFarBlipped] > A[i+1]) {
-                        maxBlippingPoint = i+1;
-                        ++i;
-                    }
-                }
-
-            } else {
-                ++i;
-            }
+        Integer minErrand = null;
+        Integer counter = 0;
+        while (counter < A.length-1 && A[counter] < A[counter+1]) {
+            ++counter;
+        }
+        if (counter == A.length) {
+            System.out.print("sorted array");
+            return;
+        } else {
+            minErrand = counter+1;
         }
 
-        Integer minSoFarBlipped = null;
-        Integer minBlippingPoint = null;
-        i=A.length-1;
-        while (i>0) {
-            if (A[i] < A[i-1]) {
-                if (minSoFarBlipped == null || A[minSoFarBlipped] > A[i]) {
-                    minSoFarBlipped = i;
+        Integer maxErrand = null;
+        counter = A.length-1;
+        while (counter > 0 && A[counter] > A[counter-1]) {
+            --counter;
+        }
+        maxErrand = counter-1;
 
-                    while (i>0 && A[minSoFarBlipped] < A[i-1]) {
-                        minBlippingPoint = i-1;
-                        --i;
-                    }
-                 }
-            } else {
-                --i;
+
+        counter = minErrand;
+        while (counter < maxErrand) {
+            if (A[counter] < A[minErrand]) {
+                minErrand = counter;
+            } else if (A[counter] > A[maxErrand]) {
+                maxErrand = counter;
             }
+
+            ++counter;
         }
 
-        System.out.print(maxBlippingPoint + " " + minBlippingPoint);
+
+        counter = 0;
+        while (counter < minErrand && A[counter] < A[minErrand]) {
+            ++counter;
+        }
+        minErrand = counter;
+
+        counter = A.length-1;
+        while (counter > maxErrand && A[counter] > A[maxErrand]) {
+            --counter;
+        }
+        maxErrand = counter;
+
+        System.out.print(minErrand + " " + maxErrand);
 
     }
 
