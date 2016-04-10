5c4b27a8fc9cadb554e475138565f89509b069f5
shanxS <shanx.shashank@gmail.com>
2016-04-10 15:40:10 +0530

    Find duplicates in O(n) time and O(1) extra space - practice

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -3,12 +3,12 @@ public class Algo {
     private Integer[] A = {1, 2, 3, 1, 3, 6, 6};
 
     public void run() {
-        for (Integer i=0; i<A.length; ++i) {
-            if (A[Math.abs(A[i])] < 0) {
-                System.out.println("repeated " + Math.abs(Math.abs(A[i])));
-            } else {
-                A[Math.abs(A[i])] = -1*A[Math.abs(A[i])];
-            }
-        }
+       for (Integer i=0; i<A.length; ++i) {
+           if (A[Math.abs(A[i])] < 0) {
+               System.out.print("repeted " + Math.abs(A[i]));
+           } else {
+               A[Math.abs(A[i])] = -1*A[Math.abs(A[i])];
+           }
+       }
     }
 }
