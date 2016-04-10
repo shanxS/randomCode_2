34d0b4ae0a1e069b31e396f6cac2fddab3f7e44e
shanxS <shanx.shashank@gmail.com>
2016-04-10 16:08:25 +0530

    countign repetitions in O(1) sapce and O(n) tiem

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -1,14 +1,23 @@
 public class Algo {
 
     private Integer[] A = {1, 2, 3, 1, 3, 6, 6};
+    private final Integer BIG = A.length;
 
     public void run() {
-       for (Integer i=0; i<A.length; ++i) {
-           if (A[Math.abs(A[i])] < 0) {
-               System.out.print("repeted " + Math.abs(A[i]));
-           } else {
-               A[Math.abs(A[i])] = -1*A[Math.abs(A[i])];
-           }
-       }
+        for (Integer i=0; i<A.length; ++i) {
+            // get orginal number
+            Integer number = A[i] % BIG;
+
+            // goto that index and increment BIG
+            A[number] += BIG;
+        }
+
+        for (Integer i=0; i<A.length; ++i) {
+            // get orginal number
+            Integer number = A[i] % BIG;
+
+            System.out.println(number + " was repeated " + ((A[number]) / BIG));
+        }
     }
+
 }
