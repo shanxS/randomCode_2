c31edbbc28ad4f7d08558217c14da7f680c8f291
shanxS <shanx.shashank@gmail.com>
2016-04-10 15:37:19 +0530

    Find duplicates in O(n) time and O(1) extra space

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -1,53 +1,14 @@
 public class Algo {
 
-//    private Integer[] A = {0, 1, 15, 25, 6, 7, 30, 40, 50};
-    private Integer[] A = {10, 12, 20, 30, 25, 40, 32, 31, 35, 50, 60};
+    private Integer[] A = {1, 2, 3, 1, 3, 6, 6};
 
     public void run() {
-
-        Integer minErrand = null;
-        Integer counter = 0;
-        while (counter < A.length-1 && A[counter] < A[counter+1]){
-            ++counter;
-        }
-        if (counter == A.length-1) {
-            System.out.print("sorted");
-        } else {
-            minErrand = counter;
-        }
-
-
-        Integer maxErrand = null;
-        counter = A.length-1;
-        while (counter > 0 && A[counter] > A[counter-1]) {
-            --counter;
-        }
-        maxErrand = counter;
-
-        counter = minErrand;
-        Integer maxErrandInit = maxErrand;
-        while (counter <= maxErrandInit) {
-            if (A[counter] < A[minErrand]) {
-                minErrand = counter;
-            } else if (A[counter] > A[maxErrand]) {
-                maxErrand = counter;
+        for (Integer i=0; i<A.length; ++i) {
+            if (A[Math.abs(A[i])] < 0) {
+                System.out.println("repeated " + Math.abs(Math.abs(A[i])));
+            } else {
+                A[Math.abs(A[i])] = -1*A[Math.abs(A[i])];
             }
-
-            ++counter;
         }
-
-        counter = 0;
-        while (counter <= minErrand && A[counter] < A[minErrand]) {
-            counter++;
-        }
-        minErrand = counter;
-
-        counter = A.length-1;
-        while (counter >= maxErrand && A[counter] > A[maxErrand]) {
-            --counter;
-        }
-        maxErrand = counter;
-
-        System.out.print(minErrand + " " + maxErrand);
     }
 }
