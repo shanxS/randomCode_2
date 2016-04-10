d51f1d48cd87400092676609cc4061249eda5575
shanxS <shanx.shashank@gmail.com>
2016-04-10 14:32:52 +0530

    minimun unsorted array in array

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -3,39 +3,49 @@
  */
 public class Algo {
 
-//    private Integer[] A = {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 2};
-//    private Integer[] A = {2,2,2,2,0};
-    private Integer[] A = {1,1,1,1};
+    private Integer[] A = {10, 12, 20, 30, 25, 40, 32, 31, 35, 50, 60};
+//    private Integer[] A = {0, 1, 15, 25, 6, 7, 30, 40, 50};
 
     public void run() {
-        Integer endMarker = A.length;
-        Integer startMarker = -1;
-        Integer marker = 0;
+        Integer maxSoFarBlipped = null;
+        Integer maxBlippingPoint = null;
+        Integer i=0;
+        while (i<A.length-1) {
+            if (A[i] > A[i + 1]) {
+                if (maxSoFarBlipped == null || A[maxSoFarBlipped] < A[i]) {
+                    maxSoFarBlipped = i;
 
-        while (marker<endMarker) {
-            if (A[marker] == 0) {
-                ++startMarker;
-                swap (startMarker, marker);
+                    while (i<A.length-1 && A[maxSoFarBlipped] > A[i+1]) {
+                        maxBlippingPoint = i+1;
+                        ++i;
+                    }
+                }
 
-                ++marker;
-            } else if (A[marker] == 2) {
-                --endMarker;
-                swap (endMarker, marker);
-            } else if (A[marker] == 1) {
-                ++marker;
+            } else {
+                ++i;
             }
         }
 
-        for (Integer ele : A) {
-            System.out.print(ele + " ");
+        Integer minSoFarBlipped = null;
+        Integer minBlippingPoint = null;
+        i=A.length-1;
+        while (i>0) {
+            if (A[i] < A[i-1]) {
+                if (minSoFarBlipped == null || A[minSoFarBlipped] > A[i]) {
+                    minSoFarBlipped = i;
+
+                    while (i>0 && A[minSoFarBlipped] < A[i-1]) {
+                        minBlippingPoint = i-1;
+                        --i;
+                    }
+                 }
+            } else {
+                --i;
+            }
         }
-    }
 
-    private void swap(Integer from, Integer to) {
-        Integer temp = A[from];
-        A[from] = A[to];
-        A[to] = temp;
-    }
+        System.out.print(maxBlippingPoint + " " + minBlippingPoint);
 
+    }
 
 }
