4ba908c6e106e48c1876b7b4a2b2a8bc430629b6
shanxS <shanx.shashank@gmail.com>
2016-04-10 20:24:49 +0530

    Turn an image by 90 degree

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -1,21 +1,32 @@
 public class Algo {
 
-    private Integer[] A = {-7, 1, 5, 2, -4, 3, 0};
+//    private Integer[][] A = {
+//            {1,2,3},
+//            {4,5,6},
+//            {7,8,9},
+//            {10,11,12}
+//    };
+
+    private Integer[][] A = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
 
     public void run() {
-        Integer total = 0;
-        for (Integer ele : A) {
-            total += ele;
+        Integer initRowCount = A.length;
+        Integer initColCount = A[0].length;
+
+        Integer[][] res = new Integer[initColCount][initRowCount];
+
+        for (Integer inputRow = 0, outputCol = initRowCount-1; inputRow < initRowCount; ++inputRow, --outputCol) {
+            for (Integer inputCol = 0, outputRow = 0; inputCol < initColCount; ++inputCol, ++outputRow) {
+                res[outputRow][outputCol] = A[inputRow][inputCol];
+            }
         }
 
-        Integer sumSoFar = 0;
-        for (Integer i=0; i<A.length; ++i) {
-            Integer ele = A[i];
-            if (sumSoFar == (total-sumSoFar-ele)) {
-                System.out.println("equilb " + i);
+        for (Integer[] arr : res) {
+            for (Integer ele : arr) {
+                System.out.print(ele + " ");
             }
 
-            sumSoFar += ele;
+            System.out.println();
         }
     }
 
