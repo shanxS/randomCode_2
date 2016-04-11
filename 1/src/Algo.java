115802af4de63b8cf0bf7652f0cb2f23cb9bd8de
shanxS <shanx.shashank@gmail.com>
2016-04-11 19:49:19 +0530

    finding messing element modified binary

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -1,19 +1,28 @@
 public class Algo {
 
-    private Integer[] A = {0, 1, 2, 6, };
+    private Integer[] A = {0, 1, 2, 3, 4, 5, 6, 7, 10};
 
     public void run() {
-        Integer ele = 0;
-        for (Integer foundEle : A) {
-            if (foundEle != ele) {
-                System.out.print("cant find " + ele);
-                return;
-            }
 
-            ++ele;
+        Integer start = 0, end = A.length-1;
+
+        Integer mid = null;
+        while (start <= end) {
+            mid = Math.min(start, end) + ((Math.abs(start - end))/2);
+
+            if (A[mid] > mid) {
+                end = mid-1;
+            } else if (A[mid] == mid) {
+                start = mid + 1;
+            }
         }
 
-        System.out.print("found all");
+        if (A[mid] == mid) {
+            System.out.print("no missing");
+        } else {
+            System.out.print("missing found at " + mid);
+        }
+
     }
 
 }
