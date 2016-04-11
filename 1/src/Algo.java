8a31286bcfeda689651a22982c5f088568504be4
shanxS <shanx.shashank@gmail.com>
2016-04-11 20:11:48 +0530

    finding coutn practice

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -3,21 +3,21 @@ public class Algo {
     private Integer[] A = {1, 1, 2, 2, 2, 2, 3};
 
     public void run() {
-        Integer target = 4;
-        Integer firstOcc = findFirstOccurance(target);
-        Integer lastOcc = findLastOccurance(target);
+        Integer target = 1;
+        Integer firstOccurance = findFirstOccurance(target);
+        Integer lastOccurance = findLastOccurance(target);
 
-
-        System.out.print(firstOcc + " " + lastOcc + " " + (lastOcc-firstOcc+1));
-
+        System.out.print(firstOccurance + " " + lastOccurance + " " + (lastOccurance-firstOccurance + 1));
     }
 
     private Integer findLastOccurance (Integer target) {
+        Integer start = 0;
+        Integer end = A.length-1;
+
         Integer mid = null;
-        Integer start = 0, end = A.length-1;
 
         while (start <= end) {
-            mid = Math.min(start, end) + ((Math.abs(start-end))/2);
+            mid = Math.min(start,end)  + ((Math.abs(start-end))/2);
 
             if (A[mid] == target) {
                 if (mid == A.length-1 || A[mid+1] != target) {
@@ -25,30 +25,29 @@ public class Algo {
                 } else {
                     start = mid + 1;
                 }
-            } else if (A[mid] < target) {
+            } if (A[mid] < target) {
                 start = mid + 1;
             } else if (A[mid] > target) {
-                end = mid-1;
+                end = mid - 1;
             }
         }
-
 
         return mid;
     }
 
-
     private Integer findFirstOccurance (Integer target) {
+        Integer start = 0;
+        Integer end = A.length-1;
         Integer mid = null;
-        Integer start = 0, end = A.length-1;
 
-        while (start <= end) {
+        while(start <= end) {
             mid = Math.min(start, end) + ((Math.abs(start-end))/2);
 
             if (A[mid] == target) {
                 if (mid == 0 || A[mid-1] != target) {
                     break;
                 } else {
-                    end = mid - 1;
+                    end = end -1;
                 }
             } else if (A[mid] < target) {
                 start = mid + 1;
@@ -57,8 +56,8 @@ public class Algo {
             }
         }
 
-        return mid;
 
+        return mid;
     }
 
 }
