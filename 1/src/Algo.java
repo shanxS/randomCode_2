3630dc23a7eb102b89907ef504ca79f88cbff33f
shanxS <shanx.shashank@gmail.com>
2016-04-11 19:41:59 +0530

    finding messing element using linear search

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -1,28 +1,19 @@
 public class Algo {
 
-    private Integer[] A = {34, 23, 52, 12, 3};
+    private Integer[] A = {0, 1, 2, 6, };
 
     public void run() {
-        Integer min = A[0];
-        Integer max = A[0];
-        Integer sum = 0;
+        Integer ele = 0;
+        for (Integer foundEle : A) {
+            if (foundEle != ele) {
+                System.out.print("cant find " + ele);
+                return;
+            }
 
-        for (Integer ele : A) {
-            sum += ele;
-
-            min = Math.min(min, ele);
-            max = Math.max(max, ele);
+            ++ele;
         }
 
-        Integer ap = (A.length*(min + max))/2;
-        if (sum == ap) {
-            System.out.print("consecutive");
-        } else {
-            System.out.print("NOT consecutive");
-        }
-
-
-
+        System.out.print("found all");
     }
 
 }
