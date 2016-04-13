1cc5399bc689e3ec9095f7454ef1b314734abacd
shanxS <shanx.shashank@gmail.com>
2016-04-13 18:23:17 +0530

    Given an array arr[], find the maximum j – i such that arr[j] > arr[i]

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -3,113 +3,49 @@ public class Algo {
     private Integer[] A = {6, 5, 4, 3, 2, 1};
 
     public void run() {
-        BST bst = new BST();
+        Integer[] lMinIndex = new Integer[A.length];
+        Integer minMarker = 0;
+        lMinIndex[minMarker] = minMarker;
+        Integer minSoFarIndex = minMarker;
 
-        for (Integer i=0; i<A.length; ++i) {
-            Integer ele = A[i];
-            bst.insert(ele, i);
+        Integer[] rMaxIndex = new Integer[A.length];
+        Integer maxMarker = A.length-1;
+        rMaxIndex[maxMarker] = maxMarker;
+        Integer maxSoFarIndex = maxMarker;
+
+        while (minMarker < A.length) {
+            if (A[minSoFarIndex] > A[minMarker]) {
+                minSoFarIndex = minMarker;
+            }
+            lMinIndex[minMarker] = minSoFarIndex;
+
+            if (A[maxSoFarIndex] < A[maxMarker]) {
+                maxSoFarIndex = maxMarker;
+            }
+            rMaxIndex[maxMarker] = maxSoFarIndex;
+
+            ++minMarker;
+            --maxMarker;
         }
 
-        System.out.print(bst.getMaxGap());
-    }
-}
-
-class BST{
-    private Node head;
-    private Integer maxGap = Integer.MIN_VALUE, start, stop;
-
-    public Integer getMaxGap() {
-        System.out.println(start + " " + stop);
-        return maxGap;
-    }
-
-
-    public void insert(Integer value, Integer selfIndex) {
-        if (head == null) {
-            head = new Node(value, selfIndex);
-        } else {
-            insert(head, value, selfIndex);
-        }
-    }
-
-    private void insert(Node node, Integer value, Integer selfIndex) {
-
-        if (node.getValue() < value) {
-
-            if (node.getRight() == null) {
-                Node rightNode = new Node(value, selfIndex);
-                rightNode.setLeftIndex(node.getLeftIndex());
-
-                if (selfIndex - node.getLeftIndex() > maxGap) {
-                    stop = node.getLeftIndex();
-                    start = selfIndex;
-                    maxGap = selfIndex - node.getLeftIndex();
+        minMarker = 0;
+        maxMarker = 0;
+        Integer gap = Integer.MIN_VALUE;
+        Integer start=null, end=null;
+        while (minMarker < A.length && maxMarker < A.length) {
+            if (A[lMinIndex[minMarker]] < A[rMaxIndex[maxMarker]]) {
+                if (gap < (rMaxIndex[maxMarker] - lMinIndex[minMarker])) {
+                    gap = (rMaxIndex[maxMarker] - lMinIndex[minMarker]);
+                    start = lMinIndex[minMarker];
+                    end = rMaxIndex[maxMarker];
                 }
 
-                node.setRight(rightNode);
+                ++maxMarker;
             } else {
-                insert(node.getRight(), value, selfIndex);
+                ++minMarker;
             }
-
-
-        } else if (node.getValue() > value) {
-
-            if (node.getLeft() == null) {
-                Node leftNode = new Node(value, selfIndex);
-                node.setLeft(leftNode);
-
-            } else {
-                insert(node.getLeft(), value, selfIndex);
-            }
-
         }
 
-
-    }
-}
-
-
-class Node {
-    private Node left, right;
-    private Integer value, leftIndex, sellIndex;
-
-    public Node(Integer value, Integer selfIndex) {
-        this.value = value;
-        this.sellIndex = selfIndex;
-    }
-
-    public Integer getSelfIndex() { return sellIndex; }
-
-    public Integer getLeftIndex() {
-        if (leftIndex == null) {
-            return sellIndex;
-        } else {
-            return leftIndex;
-        }
-
-    }
-
-    public void setLeft(Node left) {
-        this.left = left;
-    }
-
-    public void setRight(Node right) {
-        this.right = right;
-    }
-
-    public void setLeftIndex(Integer leftIndex) {
-        this.leftIndex = leftIndex;
-    }
-
-    public Node getLeft() {
-        return left;
-    }
-
-    public Node getRight() {
-        return right;
-    }
-
-    public Integer getValue() {
-        return value;
+        System.out.print(start + " " + end + " " + gap);
     }
 }
