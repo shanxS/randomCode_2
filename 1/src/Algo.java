76d7fd90dc1ce5bae2c0705201cad3f3389811b2
shanxS <shanx.shashank@gmail.com>
2016-04-14 20:47:55 +0530

    Find the minimum distance between two numbers

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -1,46 +1,28 @@
-import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Algo {
 
-    private Integer[] A = {1, 2, 3, 1, 4, 5, 2, 3, 6};
-    private ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
-    private final Integer size = 3;
+    private Integer[] A = {2, 5, 3, 5, 4, 4, 2, 3};
+    private Map<Integer, Integer> map = new HashMap<>();
+    private Integer key1 = 3, key2 = 2, minDistance = Integer.MAX_VALUE;
 
     public void run() {
-        init();
-        processForOthers();
-    }
+        map.put(key1, null);
+        map.put(key2, null);
 
-    private void processForOthers() {
-        Integer counter = size;
+        for (Integer i=0; i<A.length; ++i) {
+            if (map.containsKey(A[i])) {
+                map.put(A[i], i);
 
-        while (counter < A.length) {
-
-            System.out.println(A[arrayDeque.getLast()]);
-
-            while (arrayDeque.size() > size) {
-                arrayDeque.removeLast();
+                Integer pos1 = map.get(key1);
+                Integer pos2 = map.get(key2);
+                if (pos1 != null && pos2 != null) {
+                    minDistance = Math.min(minDistance, Math.abs(pos1 - pos2));
+                }
             }
-
-            while (arrayDeque.size() > 0 && A[counter] >= A[arrayDeque.peekLast()]) {
-                arrayDeque.removeLast();
-            }
-
-            arrayDeque.addFirst(counter);
-            ++counter;
         }
-    }
 
-    private void init() {
-        Integer counter = 0;
-
-        while (counter < size) {
-            while (arrayDeque.size() > 0 && A[counter] >= A[arrayDeque.peekLast()]) {
-                arrayDeque.removeLast();
-            }
-
-            arrayDeque.addFirst(counter);
-            ++counter;
-        }
+        System.out.print(minDistance);
     }
 }
