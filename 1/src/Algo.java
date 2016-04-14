821283fc53cddcddd1ab3fddac4fdf55ece10701
shanxS <shanx.shashank@gmail.com>
2016-04-14 21:17:01 +0530

    Find the repeating and the missing marking same array

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -1,28 +1,21 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Algo {
 
-    private Integer[] A = {2, 5, 3, 5, 4, 4, 2, 3};
-    private Map<Integer, Integer> map = new HashMap<>();
-    private Integer key1 = 3, key2 = 2, minDistance = Integer.MAX_VALUE;
+    private Integer[] A = {4, 3, 6, 2, 1, 1};
 
     public void run() {
-        map.put(key1, null);
-        map.put(key2, null);
 
-        for (Integer i=0; i<A.length; ++i) {
-            if (map.containsKey(A[i])) {
-                map.put(A[i], i);
-
-                Integer pos1 = map.get(key1);
-                Integer pos2 = map.get(key2);
-                if (pos1 != null && pos2 != null) {
-                    minDistance = Math.min(minDistance, Math.abs(pos1 - pos2));
-                }
+        for(Integer i=0; i<A.length; ++i) {
+            if (A[Math.abs(A[i]) - 1] < 0) {
+                System.out.print("repeated " + Math.abs(A[i]));
+            } else {
+                A[Math.abs(A[i]) - 1] = -A[Math.abs(A[i]) - 1];
             }
         }
 
-        System.out.print(minDistance);
+        for(Integer i=0; i<A.length; ++i) {
+            if (A[i] > 0) {
+                System.out.print("not found " + (i+1));
+            }
+        }
     }
 }
