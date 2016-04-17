c159a2ca899001cceb4344f8abde9df3a0fb85f7
shanxS <shanx.shashank@gmail.com>
2016-04-17 20:54:08 +0530

    min and second min

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -1,17 +1,16 @@
 import java.util.*;
-
 public class Algo {
 
     private Integer[] A = {2,3,9,4,5,6,1};
     private Stack<Integer> minStack;
     private HashMap<Integer, Integer> trace;
 
-
     public void run() {
         init();
         process();
 
     }
+
 
     private void process() {
         Stack<Integer> secMinStack = new Stack<>();
@@ -21,14 +20,15 @@ public class Algo {
         while ((isEvenRun && minStack.size() > 1)
                 || (!isEvenRun && secMinStack.size() > 1)) {
 
+
             if (isEvenRun) {
 
                 while (minStack.size() > 1) {
-                    secMinStack.push( compete (minStack.pop(), minStack.pop()));
+                    secMinStack.push ( compete (minStack.pop(), minStack.pop()) );
                 }
 
                 if (minStack.size() == 1) {
-                    secMinStack.push(minStack.pop());
+                    secMinStack.push ( minStack.pop() );
                 }
 
             } else {
@@ -38,14 +38,15 @@ public class Algo {
                 }
 
                 if (secMinStack.size() == 1) {
-                    minStack.push (secMinStack.pop());
+                    minStack.push(secMinStack.pop());
                 }
 
             }
 
-            isEvenRun = !isEvenRun;
 
+            isEvenRun = !isEvenRun;
         }
+
 
         if (isEvenRun) {
             System.out.print("min " + minStack.peek() + " sec min " + trace.get(minStack.peek()));
@@ -56,34 +57,33 @@ public class Algo {
     }
 
     private void init() {
-
         minStack = new Stack<>();
-        trace = new HashMap();
+        trace = new HashMap<>();
 
         Integer counter = 0;
         while (counter < A.length) {
 
             if (counter+1 < A.length) {
-                minStack.push ( compete (A[counter], A[counter+1] ) );
+                minStack.push ( compete (A[counter], A[counter+1]) );
             } else {
-                minStack.push(A[counter]);
+                minStack.push (A[counter]);
             }
+
 
             counter += 2;
         }
-
 
     }
 
     private Integer compete(Integer i1, Integer i2) {
         Integer min = Math.min(i1, i2);
-        Integer max = Math.max (i1, i2);
+        Integer max = Math.max(i1, i2);
 
         Integer compe = trace.get(min);
         if (compe == null) {
             compe = Integer.MAX_VALUE;
         }
-        trace.put (min, Math.min(compe, max));
+        trace.put(min, Math.min(compe, max));
 
         return min;
     }
