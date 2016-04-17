373b0f1e896148a0c3418bb56d307384ae7578e6
shanxS <shanx.shashank@gmail.com>
2016-04-17 20:23:12 +0530

    min and second min

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -1,87 +1,91 @@
-import java.util.HashMap;
-import java.util.Stack;
+import java.util.*;
 
 public class Algo {
 
-    private Integer[] A = {2,3,9,4,5,6};
-    private Stack<Integer> maxStack;
-    private HashMap<Integer, Integer> compe;
+    private Integer[] A = {2,3,9,4,5,6,1};
+    private Stack<Integer> minStack;
+    private HashMap<Integer, Integer> trace;
 
     public void run() {
-        init();
-        process();
 
+        init ();
+        process();
 
     }
 
     private void process() {
-        Stack<Integer> secMaxInteger = new Stack<>();
+        Stack<Integer> secMinStack = new Stack<>();
 
         Boolean isEvenRun = true;
 
-        while ((isEvenRun && maxStack.size() > 1)
-                || (!isEvenRun && secMaxInteger.size() > 1)) {
+        while ((isEvenRun && minStack.size() > 1)
+                || (!isEvenRun && secMinStack.size() > 1)) {
 
             if (isEvenRun) {
 
-                while (maxStack.size() > 1) {
-                    secMaxInteger.push(compete(maxStack.pop(), maxStack.pop()));
+                while (minStack.size() > 1) {
+                    secMinStack.push ( compete (minStack.pop(), minStack.pop()));
                 }
 
-                if (maxStack.size() == 1) {
-                    secMaxInteger.push(maxStack.pop());
+                if (minStack.size() == 1) {
+                    secMinStack.push (minStack.pop());
                 }
 
             } else {
 
-                while (secMaxInteger.size() > 1) {
-                    maxStack.push(compete(secMaxInteger.pop(), secMaxInteger.pop()));
+                while (secMinStack.size() > 1) {
+                    minStack.push( compete(secMinStack.pop(), secMinStack.pop()));
                 }
 
-                if (secMaxInteger.size() == 1) {
-                    maxStack.push(secMaxInteger.pop());
+                if (secMinStack.size() == 1) {
+                    minStack.push(secMinStack.pop());
                 }
 
             }
 
             isEvenRun = !isEvenRun;
-
         }
 
         if (isEvenRun) {
-            System.out.print("max " + maxStack.peek() + " sec max " + compe.get(maxStack.peek()));
+            System.out.print("min " + minStack.peek() + " sec min " + trace.get(minStack.peek()));
         } else {
-            System.out.print("max " + secMaxInteger.peek() + " sec max " + compe.get(secMaxInteger.peek()));
+            System.out.print("min " + secMinStack.peek() + " sec min " + trace.get(secMinStack.peek()));
         }
     }
 
     private void init() {
-        maxStack = new Stack<>();
-        compe = new HashMap<>();
+        minStack = new Stack<>();
+        trace = new HashMap<>();
+
 
         Integer counter = 0;
         while (counter < A.length) {
-            if (counter+1  < A.length) {
-                maxStack.push(compete(A[counter], A[counter+1]));
+
+            if (counter+1 < A.length) {
+                minStack.push( compete( A[counter], A[counter+1]));
             } else {
-                maxStack.push(A[counter]);
+                minStack.push( A[counter]);
             }
+
 
             counter += 2;
         }
     }
 
-    private Integer compete(Integer d1, Integer d2) {
-        Integer big = (d1 > d2) ? d1 : d2;
-        Integer small = (d1 < d2) ? d1 : d2;
 
-        Integer comp = compe.get(big);
-        if (comp == null) {
-            comp = Integer.MIN_VALUE;
+    private Integer compete (Integer i1, Integer i2) {
+        Integer min = Math.min(i1, i2);
+        Integer max = Math.max(i1, i2);
+
+        Integer compe = trace.get(min);
+        if (compe == null) {
+            compe = Integer.MAX_VALUE;
         }
-        compe.put(big, Math.max(comp, small));
+        trace.put(min, Math.min(compe, max));
 
-        return big;
+        return min;
     }
+
+
 }
 
