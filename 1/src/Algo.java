733fbd1b18a30aa4a21ca5de40a2f3d72c314ea2
shanxS <shanx.shashank@gmail.com>
2016-04-17 15:58:28 +0530

    tournament to find second greatest prac

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -5,62 +5,64 @@ public class Algo {
 
     private Integer[] A = {2,3,9,4,5,6};
     private Stack<Integer> maxStack;
-    private HashMap<Integer, Integer> competitor;
+    private HashMap<Integer, Integer> compe;
 
     public void run() {
         init();
         process();
+
+
     }
 
     private void process() {
-        Stack<Integer> secMaxStack = new Stack<>();
+        Stack<Integer> secMaxInteger = new Stack<>();
 
         Boolean isEvenRun = true;
+
         while ((isEvenRun && maxStack.size() > 1)
-                || (!isEvenRun && secMaxStack.size() > 1)) {
+                || (!isEvenRun && secMaxInteger.size() > 1)) {
 
             if (isEvenRun) {
 
                 while (maxStack.size() > 1) {
-                    secMaxStack.push(compare(maxStack.pop(), maxStack.pop()));
+                    secMaxInteger.push(compete(maxStack.pop(), maxStack.pop()));
                 }
 
                 if (maxStack.size() == 1) {
-                    secMaxStack.push(maxStack.pop());
+                    secMaxInteger.push(maxStack.pop());
                 }
 
             } else {
 
-                while (secMaxStack.size() > 1) {
-                    maxStack.push(compare(secMaxStack.pop(), secMaxStack.pop()));
+                while (secMaxInteger.size() > 1) {
+                    maxStack.push(compete(secMaxInteger.pop(), secMaxInteger.pop()));
                 }
 
-                if (secMaxStack.size() == 1) {
-                    maxStack.push(secMaxStack.pop());
+                if (secMaxInteger.size() == 1) {
+                    maxStack.push(secMaxInteger.pop());
                 }
 
             }
 
-
             isEvenRun = !isEvenRun;
+
         }
 
         if (isEvenRun) {
-            System.out.print("max " + maxStack.peek() + " sec max " + competitor.get(maxStack.peek()));
+            System.out.print("max " + maxStack.peek() + " sec max " + compe.get(maxStack.peek()));
         } else {
-            System.out.print("max " + secMaxStack.peek() + " sec Max " + competitor.get(secMaxStack.peek()));
+            System.out.print("max " + secMaxInteger.peek() + " sec max " + compe.get(secMaxInteger.peek()));
         }
     }
 
     private void init() {
         maxStack = new Stack<>();
-        competitor = new HashMap<>();
+        compe = new HashMap<>();
 
         Integer counter = 0;
         while (counter < A.length) {
-
-            if (counter+1 < A.length) {
-                maxStack.push(compare(A[counter], A[counter+1]));
+            if (counter+1  < A.length) {
+                maxStack.push(compete(A[counter], A[counter+1]));
             } else {
                 maxStack.push(A[counter]);
             }
@@ -69,17 +71,17 @@ public class Algo {
         }
     }
 
-    private Integer compare(Integer d1, Integer d2) {
-        Integer big = (d1>d2) ? d1 : d2;
-        Integer small = (d1>d2) ? d2 : d1;
+    private Integer compete(Integer d1, Integer d2) {
+        Integer big = (d1 > d2) ? d1 : d2;
+        Integer small = (d1 < d2) ? d1 : d2;
 
-        Integer comp = competitor.get(big);
+        Integer comp = compe.get(big);
         if (comp == null) {
             comp = Integer.MIN_VALUE;
         }
-        competitor.put(big, Math.max(small, comp));
+        compe.put(big, Math.max(comp, small));
 
         return big;
     }
-
 }
+
