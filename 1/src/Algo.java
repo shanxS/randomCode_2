455a14999d482ac0bc0a44b013c49237a1facd76
shanxS <shanx.shashank@gmail.com>
2016-04-20 19:04:49 +0530

    Implement two stacks in an array

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -1,38 +1,76 @@
 public class Algo {
 
-    private Integer[] A = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
-    private Integer[] minJumps = new Integer[A.length];
-    private final Integer INF = Integer.MAX_VALUE;
+    private Integer[] stack = new Integer[5];
+    private Integer top1=-1, top2=stack.length;
+
+    private Integer[] A = {1,2,3,4,5,6};
 
     public void run() {
 
-        for (Integer i=A.length-1; i>=0;--i) {
-            findMinJumps(i);
-        }
-
-        System.out.println(minJumps[0]);
-
-        for (Integer ele : minJumps) {
-            System.out.print(ele + " ");
-        }
-
-
-    }
-
-    private void findMinJumps(Integer source) {
-
-        if ((A.length - source) <= A[source]) {
-            minJumps[source] = 1;
-        } else {
-            minJumps[source] = INF;
-        }
-
-        Integer minJump = minJumps[source];
-        for (Integer i=1; i<=A[source] && (i+source) < A.length; ++i) {
-            if (minJumps[i+source] != INF && 1 + minJumps[i+source] < minJump) {
-                minJump = 1+minJumps[i+source];
+        Boolean flag = true;
+        for (Integer ele : A) {
+            if (flag) {
+                System.out.print(ele + " " + push1(ele));
+            } else {
+                System.out.print(ele + " " + push2(ele));
             }
+
+            flag = !flag;
         }
-        minJumps[source] = minJump;
+
+        System.out.println();
+
+        Integer data;
+
+        while ((data = pop1()) != null) {
+            System.out.println(data);
+        }
+
+        System.out.println();
+
+        while ((data = pop2()) != null) {
+            System.out.println(data);
+        }
     }
+
+    public Boolean push1 (Integer x) {
+        if (top1 + 1 < top2) {
+            ++top1;
+            stack[top1] = x;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public Integer pop1() {
+        if (top1 >= 0) {
+            --top1;
+            return stack[top1+1];
+        }
+
+        return null;
+    }
+
+    public boolean push2 (Integer x) {
+        if (top2-1 > top1) {
+            --top2;
+            stack[top2] = x;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public Integer pop2() {
+        if (top2 < stack.length) {
+            ++top2;
+            return stack[top2-1];
+        }
+
+        return null;
+    }
+
 }
