29d1054782bb0423ddbc08340a14f272f04863dc
shanxS <shanx.shashank@gmail.com>
2016-04-21 21:40:56 +0530

    Longest Bitonic Subsequence - disontinous - nlogn using bst

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -1,54 +1,156 @@
 public class Algo {
 
-    private Integer[] A = {80, 60, 30, 40, 20, 10};
-    private Integer[] LIS = new Integer[A.length];
-    private Integer[] LDS = new Integer[A.length];
+    private Integer[] A = {1, 11, 2, 10, 4, 5, 2, 1};
 
     public void run() {
-        populateLIS();
-        populateLDS();
-        System.out.println(findLongestBitonic());
 
-        for (Integer ele : LIS) {
-            System.out.print(ele + " ");
-        }
-
-        System.out.println();
-        for (Integer ele : LDS) {
-            System.out.print(ele + " ");
-        }
-   }
-
-    private Integer findLongestBitonic() {
-        Integer num = Integer.MIN_VALUE;
-
-        for (Integer i=0; i<A.length; ++i) {
-            num = Math.max((LDS[i] + LIS[i] - 1), num);
-        }
-
-        return num;
-    }
-
-    private void populateLDS() {
+        BST bst = new BST();
         for (Integer i=A.length-1; i>=0; --i) {
-            LDS[i] = 1;
-            for (Integer j=A.length-1; j>i; --j) {
-                if (A[i] > A[j] && (LDS[i] < (LDS[j]+1))) {
-                    LDS[i] = 1+LDS[j];
-                }
+            bst.insert(A[i]);
+        }
+
+        bst.print();
+
+        System.out.print(bst.findMaxBitonic());
+
+   }
+}
+
+class BST {
+    private Node head;
+
+    public void insert(Integer value) {
+
+        if (head == null) {
+            head = new Node(value, 0);
+        } else {
+            insert(value, head, head.getDec());
+        }
+
+    }
+
+    public void print() {
+        print(head);
+    }
+
+    private void print(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.print(node.getValue() + " d:" + node.getDec() + " i:" + node.getInc() + " - ");
+        if (node.getLeft() != null) {
+            System.out.print(node.getLeft().getValue());
+        }
+        System.out.print(", ");
+        if (node.getRight() != null) {
+            System.out.print(node.getRight().getValue());
+        }
+        System.out.println();
+
+        print(node.getLeft());
+        print(node.getRight());
+    }
+
+    private void insert(Integer value, Node node, Integer dec) {
+        if (node.getValue() < value) {
+
+            Integer maxDec = Math.max(dec, node.getDec());
+
+            if (node.getRight() == null) {
+                node.setRight(new Node(value, maxDec));
+            } else {
+
+                insert(value, node.getRight(), maxDec);
+
             }
+
+        } else {
+
+            node.bumpInc();
+            bumpRights(node.getRight());
+
+            if (node.getLeft() == null) {
+                node.setLeft(new Node(value, dec));
+            } else {
+                insert(value, node.getLeft(), dec);
+            }
+
         }
     }
 
-    private void populateLIS() {
-
-        for (Integer i=0; i<A.length; ++i) {
-            LIS[i] = 1;
-            for (Integer j=0; j<i; ++j) {
-                if (A[j] < A[i] &&  (LIS[i] <  (1+LIS[j]))) {
-                    LIS[i] = 1+LIS[j];
-                }
-            }
+    private void bumpRights(Node node) {
+        if (node == null) {
+            return;
         }
+
+        node.bumpInc();
+        bumpRights(node.getLeft());
+        bumpRights(node.getRight());
+    }
+
+
+    private Integer maxBitonic;
+    public Integer findMaxBitonic() {
+        maxBitonic = Integer.MIN_VALUE;
+        find(head);
+
+        return maxBitonic;
+    }
+
+    private void find(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        maxBitonic = Math.max(maxBitonic, (node.getInc() + node.getDec()));
+        find(node.getLeft());
+        find(node.getRight());
+    }
+}
+
+class Node {
+    private Integer value;
+    private Integer inc;
+    private Integer dec;
+
+    private Node left, right;
+
+    public Node(Integer value, Integer dec) {
+        this.value = value;
+        this.dec = dec + 1;
+        this.inc = 0;
+    }
+
+    public void bumpInc() {
+        ++inc;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public Integer getInc() {
+        return inc;
+    }
+
+    public Integer getDec() {
+        return dec;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
     }
 }
