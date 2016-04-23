ccca0893a7c8208bc8f037a9d60fcfff041d09a9
shanxS <shanx.shashank@gmail.com>
2016-04-23 21:18:25 +0530

    Find four elements that sum to a given value - n^2*logn

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -4,81 +4,45 @@ public class Algo {
 
     private Integer[] A = {10, 2, 3, 4, 5, 9, 7, 8};
     private Integer target = 23;
+    private Map<Integer, Node> sumMap;
 
     public void run() {
 
-        sort(0, A.length-1);
+        init();
 
+        for (Integer i = A.length - 1; i >= 3; --i) {
+            for (Integer j = i - 1; j >= 2; --j) {
 
-        for (Integer i=A.length-1; i>=3; --i) {
-            for (Integer j=i-1; j>=2; --j) {
                 Integer diff = target - (A[i] + A[j]);
-                Integer fwd = 0, rev = j-1;
-                while (fwd < rev) {
-                    Integer sum = A[rev] + A[fwd];
-                    if (sum == diff) {
-                        System.out.println(A[i] + " " + A[j] + " " + A[fwd] + " " + A[rev]);
-                        break;
-                    } else if (sum < diff) {
-                        ++fwd;
-                    } else if (sum > diff) {
-                        --rev;
-                    }
+                Node node = sumMap.get(diff);
+                if (node != null) {
+                    System.out.println(A[i] + " " + A[j] + " " + node.num1 + " " + node.num2);
                 }
             }
         }
-
     }
 
-    private Integer find(Integer diff, Integer start, Integer end) {
-        while (start <= end) {
+    private void init() {
+        sumMap = new HashMap<>();
 
-            Integer mid = Math.min(start, end) + ((Math.abs(A[start]-A[end]))/2);
-
-            if (A[mid] == diff) {
-                return mid;
-            } else if (A[mid] < diff) {
-                start = mid+1;
-            } else {
-                end = mid-1;
-            }
-
-        }
-
-        return null;
-    }
-
-    public void sort(Integer start, Integer end) {
-
-        if (start >= end) {
-            return;
-        }
-
-        Integer pivot = qSort(start, end);
-        sort(start, pivot-1);
-        sort(pivot+1, end);
-
-    }
-
-    private Integer qSort(Integer start, Integer end) {
-        Integer pivotValue = A[end];
-        Integer marker = start-1;
-
-        for (Integer i=start; i<end; ++i) {
-            if (A[i] <= pivotValue) {
-                ++marker;
-                swap (marker, i);
+        for (Integer i=0; i<A.length; ++i) {
+            for (Integer j=i+1; j<A.length; ++j) {
+                Integer sum = A[i] + A[j];
+                sumMap.put(sum, new Node(A[i], A[j], i, j));
             }
         }
 
-        swap(marker+1, end);
-        return marker+1;
     }
+}
 
-    private void swap (Integer from, Integer to) {
-        Integer tmp = A[from];
-        A[from] = A[to];
-        A[to] = tmp;
+class Node {
+    public Integer num1, num2;
+    public Integer index1, index2;
+
+    public Node(Integer num1, Integer num2, Integer index1, Integer index2) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.index1 = index1;
+        this.index2 = index2;
     }
-
 }
