1a4b5184cfcf00cf2fcdb41331490a14729c1cbb
shanxS <shanx.shashank@gmail.com>
2016-04-26 20:01:36 +0530

    Find Union and Intersection of two unsorted arrays

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,65 +1,115 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinaryCode {
 
-    private Integer[] A = {3, 1, 4, 6, 5};
+    private Integer[] A = {7, 1, 5, 2, 3, 6};
+    private Integer[] B = {3, 8, 6, 20, 7};
 
     public void run() {
+        BST bst = new BST();
 
-        sort(0, A.length-1);
-
-        for (Integer i=0; i<A.length; ++i) {
-            A[i] = (int) Math.pow(A[i], 2);
+        for (Integer ele : A) {
+            bst.insert(ele);
+        }
+        for (Integer ele : B) {
+            bst.insert(ele);
         }
 
-        for (Integer i=A.length-1; i>=2; --i) {
-            Integer start = 0;
-            Integer end = i-1;
-            while (start < end) {
-                if (A[start] + A[end] == A[i]) {
-                    System.out.print(A[start] + " " + A[end] + " " + A[i]);
-
-                    ++start;
-                    --end;
-                } else if (A[start] + A[end] < A[i]) {
-                    ++start;
-                } else if (A[start] + A[end] > A[i]) {
-                    --end;
-                }
-            }
-        }
+        bst.printIntersection();
+        System.out.println("-----");
+        bst.printUnion();
 
     }
+}
 
+class BST {
+    private Node head;
+    private List<Integer> intersection;
 
-    private void sort(Integer start, Integer end) {
-        if (start >= end) {
+    public BST() {
+        intersection = new ArrayList<>();
+    }
+
+    public void insert(Integer value) {
+        if (head == null) {
+            head = new Node(value);
+        } else {
+            insert(head, value);
+        }
+    }
+
+    public void printIntersection() {
+        intersection.forEach(val -> System.out.print(val + " "));
+    }
+
+    public void printUnion() {
+        print(head);
+    }
+
+    private void print(Node node) {
+        if (node == null) {
             return;
         }
 
-        Integer pivot = qSort(start, end);
-        sort(start, pivot-1);
-        sort(pivot+1, end);
-    }
-
-    private Integer qSort(Integer start, Integer end) {
-
-        Integer pivotValue = A[end];
-        Integer marker = start-1;
-
-        for (Integer i=start; i<end; ++i) {
-            if (A[i] <= pivotValue) {
-                ++marker;
-                swap(marker, i);
-            }
+        System.out.print(node.getValue() + " ");
+        if (node.getLeft() != null) {
+            System.out.print(node.getLeft().getValue());
         }
+        System.out.print(", ");
+        if (node.getRight() != null) {
+            System.out.print(node.getRight().getValue());
+        }
+        System.out.println();
 
-        swap(marker+1, end);
-        return marker+1;
+        print(node.getLeft());
+        print(node.getRight());
     }
 
-    private void swap(Integer from, Integer to) {
-        Integer tmp = A[from];
-        A[from] = A[to];
-        A[to] = tmp;
+    private void insert(Node node, Integer value) {
+        if (node.getValue() > value) {
+            if (node.getLeft() == null) {
+                node.setLeft(new Node(value));
+            } else {
+                insert(node.getLeft(), value);
+            }
+        } else if (node.getValue() < value) {
+            if (node.getRight() == null) {
+                node.setRight(new Node(value));
+            } else {
+                insert(node.getRight(), value);
+            }
+        } else {
+            intersection.add(value);
+        }
+    }
+}
+
+class Node {
+    private Node left, right;
+    private Integer value;
+
+    public Node(Integer value) {
+        this.value = value;
     }
 
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
 }
