efa511dd9c478ad0208ea78c34479d322c9d86a8
shanxS <shanx.shashank@gmail.com>
2016-04-26 19:14:40 +0530

    Maximum profit by buying and selling a share at most twice - practise 2

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -8,26 +8,22 @@ public class BinaryCode {
         first[0] = 0;
         Integer minBuyValue = A[0];
         for (Integer i=1; i<A.length; ++i) {
-
             Integer thisProfit = A[i] - minBuyValue;
             first[i] = Math.max(first[i-1], thisProfit);
 
-            minBuyValue = Math.min(minBuyValue, A[i]);
+            minBuyValue = Math.min(A[i], minBuyValue);
         }
 
         Integer maxSellValue = A[A.length-1];
         Integer totalProfit = Integer.MIN_VALUE;
         for (Integer i=A.length-1; i>0; --i) {
-
             Integer thisProfit = maxSellValue - A[i];
-            totalProfit = Math.max(totalProfit, (first[i-1] + thisProfit));
+            totalProfit = Math.max(totalProfit, thisProfit+first[i-1]);
 
             maxSellValue = Math.max(maxSellValue, A[i]);
-
         }
 
         System.out.print(totalProfit);
-
     }
 
 }
