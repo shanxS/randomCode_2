635878de5b789698aa83625ca69f93afad17ecb2
shanxS <shanx.shashank@gmail.com>
2016-04-26 21:17:00 +0530

    Online algorithm for checking palindrome in a stream

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,115 +1,55 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class BinaryCode {
 
-    private Integer[] A = {7, 1, 5, 2, 3, 6};
-    private Integer[] B = {3, 8, 6, 20, 7};
+    private String str = "aabaacaabaa";
+    private Character interim = null;
+    private Integer interimPos = null;
+    private Integer fHash = 0;
+    private Integer lHash = 0;
 
     public void run() {
-        BST bst = new BST();
 
-        for (Integer ele : A) {
-            bst.insert(ele);
+
+        for (Integer i=0; i<str.length(); ++i) {
+            Character c = str.charAt(i);
+
+            updateHash(c, i);
+
+            System.out.println("character " + c + " " + ((int)c) + " " + (((fHash.equals(lHash)) || (lHash == 0)) ? "yes" : "no") + " " + fHash + " " + lHash);
+
         }
-        for (Integer ele : B) {
-            bst.insert(ele);
-        }
-
-        bst.printIntersection();
-        System.out.println("-----");
-        bst.printUnion();
-
-    }
-}
-
-class BST {
-    private Node head;
-    private List<Integer> intersection;
-
-    public BST() {
-        intersection = new ArrayList<>();
     }
 
-    public void insert(Integer value) {
-        if (head == null) {
-            head = new Node(value);
+    private void updateHash(Character c, Integer pos) {
+
+        final Integer PRIME = 31;
+
+        if (fHash == 0) {
+            fHash = PRIME * ((int) c);
+        } else if (lHash == 0) {
+            lHash = PRIME * ((int) c);
         } else {
-            insert(head, value);
-        }
-    }
+            Integer midPos = pos/2;
 
-    public void printIntersection() {
-        intersection.forEach(val -> System.out.print(val + " "));
-    }
+            if (interim != null) {
 
-    public void printUnion() {
-        print(head);
-    }
+                fHash += PRIME * ((int) interim);
+                fHash += PRIME * interimPos;
+                interim = null;
+                interimPos = null;
 
-    private void print(Node node) {
-        if (node == null) {
-            return;
-        }
-
-        System.out.print(node.getValue() + " ");
-        if (node.getLeft() != null) {
-            System.out.print(node.getLeft().getValue());
-        }
-        System.out.print(", ");
-        if (node.getRight() != null) {
-            System.out.print(node.getRight().getValue());
-        }
-        System.out.println();
-
-        print(node.getLeft());
-        print(node.getRight());
-    }
-
-    private void insert(Node node, Integer value) {
-        if (node.getValue() > value) {
-            if (node.getLeft() == null) {
-                node.setLeft(new Node(value));
+                lHash += PRIME * (pos-midPos-1);
             } else {
-                insert(node.getLeft(), value);
+
+                Character midChar = str.charAt(midPos);
+                lHash -= PRIME * ((int) midChar);
+
+                interimPos = midPos;
+                interim = midChar;
+
             }
-        } else if (node.getValue() < value) {
-            if (node.getRight() == null) {
-                node.setRight(new Node(value));
-            } else {
-                insert(node.getRight(), value);
-            }
-        } else {
-            intersection.add(value);
+
+            lHash += PRIME * ((int) c);
         }
-    }
-}
 
-class Node {
-    private Node left, right;
-    private Integer value;
-
-    public Node(Integer value) {
-        this.value = value;
-    }
-
-    public Node getLeft() {
-        return left;
-    }
-
-    public void setLeft(Node left) {
-        this.left = left;
-    }
-
-    public Node getRight() {
-        return right;
-    }
-
-    public void setRight(Node right) {
-        this.right = right;
-    }
-
-    public Integer getValue() {
-        return value;
     }
 }
