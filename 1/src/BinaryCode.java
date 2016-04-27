33b2e6593de20967c3d699bc15e80edbb9905bc2
shanxS <shanx.shashank@gmail.com>
2016-04-27 20:13:13 +0530

    Replace every array element by multiplication of previous and next - with exceptions

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -3,26 +3,22 @@ public class BinaryCode {
     private Integer[] A = {2, 3, 4, 5, 6};
     public void run() {
 
-        Integer[] res = new Integer[A.length];
-        Integer counter = 1;
-        Integer prodSoFar = 1;
-        while (counter < A.length) {
-            res[counter] = A[counter-1] * prodSoFar;
+        Integer prev = 1;
+        for (Integer i=0; i<A.length; ++i) {
+            Integer nextPrev = A[i];
 
-            prodSoFar = res[counter];
-            ++counter;
+            if (i==0) {
+                A[i] *= A[i+1];
+            } else if (i == A.length-1) {
+                A[i] *= prev;
+            } else {
+                A[i] = prev * A[i+1];
+            }
+
+            prev = nextPrev;
         }
 
-        counter = A.length-2;
-        prodSoFar = 1;
-        while (counter >= 0) {
-            res[counter] = A[counter+1] * prodSoFar;
-
-            prodSoFar = res[counter];
-            --counter;
-        }
-
-        for (Integer ele : res) {
+        for (Integer ele : A) {
             System.out.print(ele + " ");
         }
 
