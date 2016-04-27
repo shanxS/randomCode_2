0c4a18bd3ccbfdb24757a3c4fb27d76e9a085e6e
shanxS <shanx.shashank@gmail.com>
2016-04-27 18:48:34 +0530

    Find the largest pair sum in an unsorted array

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,57 +1,24 @@
 public class BinaryCode {
 
-    private String str = "aabaacaabaa";
-    private Integer interimPos = null;
-    private Character interim = null;
-    private Integer fHash = 0;
-    private Integer sHash = 0;
+    private Integer[] A = {12, 34, 10, 6, 40};
 
     public void run() {
 
-        for (Integer i=0; i<str.length(); ++i) {
+        Integer max = Integer.MIN_VALUE;
+        Integer sMax = Integer.MIN_VALUE;
 
-            Character c = str.charAt(i);
+        for (Integer ele : A) {
 
-            updateHash(c, i);
-
-            System.out.println(c + " " + ((fHash.equals(sHash) || sHash.equals(0)) ? "yes" : "no") + " " + fHash + " " + sHash);
-
-        }
-
-    }
-
-    private void updateHash(Character c, Integer pos) {
-
-        final Integer PRIME = 31;
-
-        if (fHash == 0) {
-            fHash = PRIME * ((int) c);
-        } else if (sHash == 0) {
-            sHash = PRIME * ((int) c);
-        } else {
-            Integer midPos = pos/2;
-
-            if (interim != null) {
-                fHash += PRIME * ((int) interim);
-                fHash += PRIME * interimPos;
-
-                interim = null;
-                interimPos = null;
-
-                sHash += PRIME * (pos-midPos-1);
-            } else {
-
-                interim = str.charAt(midPos);
-                interimPos = midPos;
-
-                sHash -= PRIME * ((int)interim);
+            if (ele > max) {
+                sMax = max;
+                max = ele;
+            } else if (ele > sMax) {
+                sMax = ele;
             }
 
-            sHash += PRIME * ((int)c);
-
         }
 
+        System.out.print(sMax + max);
+
     }
-
-
 }
