3dabe8df2f693ea2948f1952b8275bfb951e1ec0
shanxS <shanx.shashank@gmail.com>
2016-04-27 19:18:20 +0530

    Delete an element from array

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,34 +1,52 @@
 public class BinaryCode {
 
-    private Integer[] A = {1, 3, 4, 7, 10};
-    private Integer target = 15;
+    private Integer[] A = {3, 1, 5, 90};
+    private Integer target = 2;
 
     public void run() {
 
-        Integer v1 = null, v2 = null;
-        Integer diff = Integer.MAX_VALUE;
-
-
-        Integer start = 0, end = A.length-1;
-        while (start < end) {
-            Integer sum = A[start] + A[end];
-
-            if (diff > (Math.abs(sum - target))) {
-                diff = Math.abs(sum - target);
-                v1 = A[start];
-                v2 = A[end];
+        if (delete()) {
+            for (Integer i=0; i<A.length; ++i) {
+                System.out.print(A[i] + " ");
             }
-
-            if (sum == target) {
-                break;
-            } else if (sum > target) {
-                --end;
-            } else if (sum < target) {
-                ++start;
-            }
+        } else {
+            System.out.print("not found");
         }
 
-        System.out.print(v1 + " " + v2);
+    }
 
+    private Boolean delete() {
+
+        Integer[] tmp = new Integer[A.length-1];
+        Boolean found = false;
+        Integer writer = tmp.length-1;
+        for (Integer reader=A.length-1; reader>=0; --reader) {
+
+            if (reader == 0 && A[reader] != target) {
+                break;
+            }
+
+            if (A[reader] == target) {
+                found = true;
+                break;
+            } else {
+                tmp[writer] = A[reader];
+            }
+
+            --writer;
+
+        }
+
+
+        if (found) {
+            while (writer >= 0) {
+                tmp[writer] = A[writer];
+                --writer;
+            }
+
+            A = tmp;
+        }
+
+        return found;
     }
 }
