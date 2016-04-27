03c518cdb1971a0078578e28deee4a147422d4bc
shanxS <shanx.shashank@gmail.com>
2016-04-27 20:47:04 +0530

    Check if a given array contains duplicate elements within k distance from each other - practice

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -3,28 +3,26 @@ import java.util.Set;
 
 public class BinaryCode {
 
-    private Integer[] A = {1, 2, 3, 4, 4};
+    private Integer[] A = {1, 2, 3, 1, 4, 5};
     private Integer k = 3;
+
     public void run() {
 
         Set<Integer> set = new HashSet<>();
         Integer counter = 0;
         while (counter <= k && counter < A.length) {
-            Integer ele = A[counter];
-            if (!set.add(ele)) {
-                System.out.println("repeated " + ele);
+            if (!set.add(A[counter])) {
+                System.out.print("repeated " + A[counter]);
             }
 
             ++counter;
         }
 
-
         while (counter < A.length) {
             set.remove(A[counter-k-1]);
 
-            Integer ele = A[counter];
-            if (!set.add(ele)) {
-                System.out.println("repeated " + ele);
+            if (!set.add(A[counter])) {
+                System.out.print("repeated " + A[counter]);
             }
 
             ++counter;
