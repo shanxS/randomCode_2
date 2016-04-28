a205fd3732426a4d532a0036bcc63285749cf921
shanxS <shanx.shashank@gmail.com>
2016-04-28 20:31:40 +0530

    Find common elements in three sorted arrays - makiing solution generic

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,53 +1,103 @@
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.TreeSet;
 
 public class BinaryCode {
 
-    private Integer ar0[] = {1, 5, 10, 20, 40, 80};
-    private Integer ar1[] = {6, 7, 20, 80, 100};
-    private Integer ar2[] = {3, 4, 15, 20, 30, 70, 80, 120};
+    private Integer[][] A = {
+        {1, 5, 10, 20, 40, 80},
+        {6, 7, 20, 80, 100},
+        {3, 4, 15, 20, 30, 70, 80, 120}
+    };
 
     public void run() {
-        List<Integer> indices = Arrays.asList(new Integer[]{0,0,0});
 
-        while (true) {
-            Integer v0 = ar0[indices.get(0)];
-            Integer v1 = ar1[indices.get(1)];
-            Integer v2 = ar2[indices.get(2)];
+        TreeSet<Node> set = new TreeSet<>();
+        HashMap<Integer, Integer> valueArr = new HashMap<>();
 
-            if (v0 == v1 && v1 == v2) {
-                System.out.print("found common " + v0);
-                break;
-            } else if (v0 < v1 && v0 < v2) {
-
-                if (indices.get(0) >= ar0.length) {
-                    System.out.print("ar0 exceeded");
-                    break;
-                } else {
-                    indices.set(0, indices.get(0) + 1);
-                }
-
-            } else if (v1 < v2 && v1 < v0) {
-
-                if (indices.get(1) >= ar1.length) {
-                    System.out.print("ar1 exceeded");
-                    break;
-                } else {
-                    indices.set(1, indices.get(1) + 1);
-                }
-
-            } else if (v2 < v0 && v2 < v1) {
-
-                if (indices.get(2) >= ar2.length) {
-                    System.out.print("ar2 exceeded");
-                    break;
-                } else {
-                    indices.set(2, indices.get(2) + 1);
-                }
-
-            }
+        Integer index = 0;
+        for (Integer i=0; i<A.length; ++i) {
+            Integer[] arr = A[i];
+            set.add(new Node(arr[index], index, i));
+            valueArr.put(arr[index], i);
         }
 
+        while (set.size() > 0) {
 
+            if (valueArr.size() == 1) {
+                System.out.print("found common " + valueArr.values().toArray()[0]);
+
+                break;
+            }
+
+
+            Node least = set.pollFirst();
+            Integer elementIndex = least.index;
+            Integer arrIndex = least.arrIndex;
+            if (A[arrIndex].length <= elementIndex+1) {
+
+                System.out.print ("out of index with arr " + arrIndex);
+                break;
+
+            } else {
+
+                valueArr.remove(least.value);
+
+                Node node = new Node(A[arrIndex][elementIndex+1], elementIndex+1, arrIndex);
+
+                set.add(node);
+                valueArr.put(node.value, arrIndex);
+
+            }
+
+        }
+
+        System.out.print(set.toString());
+
+    }
+}
+
+class Node implements Comparable<Node> {
+
+    public Integer value, index, arrIndex;
+
+    public Node(Integer value, Integer index, Integer arrIndex) {
+        this.value = value;
+        this.index = index;
+        this.arrIndex = arrIndex;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        if (o.value < value) {
+            return 1;
+        } else if (o.value > value) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Node)) return false;
+
+        Node node = (Node) o;
+
+        if (!value.equals(node.value)) return false;
+        return true;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = value.hashCode();
+        result = 31 * result + index.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "arrIndex " + arrIndex + ", index " + index + ", value " + value;
     }
 }
