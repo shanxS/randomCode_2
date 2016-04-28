b335697d47c5853bb684cdb3b599cc5446a674ed
shanxS <shanx.shashank@gmail.com>
2016-04-28 21:50:44 +0530

    Construct an array from its pair-sum array - litte differnt than what ques says - question is not clearly mentioned

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,103 +1,52 @@
-import java.util.HashMap;
-import java.util.TreeSet;
-
 public class BinaryCode {
 
-    private Integer[][] A = {
-        {1, 5, 10, 20, 40, 80},
-        {6, 7, 20, 80, 100},
-        {3, 4, 15, 20, 30, 70, 80, 120}
-    };
+    private Integer[] S = {15, 13, 11, 10, 12, 10, 9, 8, 7, 5};
+    private Integer N;
+    private Integer[] A;
 
     public void run() {
 
-        TreeSet<Node> set = new TreeSet<>();
-        HashMap<Integer, Integer> valueArr = new HashMap<>();
+        findN();
+        System.out.println("N " + N);
 
-        Integer index = 0;
-        for (Integer i=0; i<A.length; ++i) {
-            Integer[] arr = A[i];
-            set.add(new Node(arr[index], index, i));
-            valueArr.put(arr[index], i);
+        findInners();
+
+        findLimits();
+
+        for (Integer ele : A) {
+            System.out.print(ele + " ");
         }
 
-        while (set.size() > 0) {
+    }
 
-            if (valueArr.size() == 1) {
-                System.out.print("found common " + valueArr.values().toArray()[0]);
+    private void findLimits() {
+        A[0] = S[0] - A[1];
+        A[N-1] = S[S.length-1] - A[A.length-2];
+    }
 
-                break;
-            }
+    private void findInners() {
 
-
-            Node least = set.pollFirst();
-            Integer elementIndex = least.index;
-            Integer arrIndex = least.arrIndex;
-            if (A[arrIndex].length <= elementIndex+1) {
-
-                System.out.print ("out of index with arr " + arrIndex);
-                break;
-
-            } else {
-
-                valueArr.remove(least.value);
-
-                Node node = new Node(A[arrIndex][elementIndex+1], elementIndex+1, arrIndex);
-
-                set.add(node);
-                valueArr.put(node.value, arrIndex);
-
-            }
-
+        A = new Integer[N];
+        Integer end = 0;
+        for (Integer i=1; i<A.length-1; ++i) {
+            end += N-i;
+            A[i] = (S[end] + S[i-1] - S[i])/2;
         }
 
-        System.out.print(set.toString());
-
-    }
-}
-
-class Node implements Comparable<Node> {
-
-    public Integer value, index, arrIndex;
-
-    public Node(Integer value, Integer index, Integer arrIndex) {
-        this.value = value;
-        this.index = index;
-        this.arrIndex = arrIndex;
     }
 
-    @Override
-    public int compareTo(Node o) {
-        if (o.value < value) {
-            return 1;
-        } else if (o.value > value) {
-            return -1;
+    private void findN() {
+
+        Integer sum = S.length;
+
+        Integer D = (int)Math.sqrt((1 + (8*sum)));
+
+        if (S.length % 2 != 0) {
+            N = (D - 1) / 2;
         } else {
-            return 0;
+            N = (D + 1) / 2;
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Node)) return false;
-
-        Node node = (Node) o;
-
-        if (!value.equals(node.value)) return false;
-        return true;
 
     }
 
-    @Override
-    public int hashCode() {
-        int result = value.hashCode();
-        result = 31 * result + index.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "arrIndex " + arrIndex + ", index " + index + ", value " + value;
-    }
 }
