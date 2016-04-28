f31b102518067dd2fac99dd2a5527bfbfb4ca3bf
shanxS <shanx.shashank@gmail.com>
2016-04-28 18:20:49 +0530

    Rearrange an array such that ‘arr[j]’ becomes ‘i’ if ‘arr[i]’ is ‘j’

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,41 +1,24 @@
 public class BinaryCode {
 
-    private Integer[] A = {3, 5, 7, 9, 10, 90, 100, 130, 140, 160, 170};
-    private Integer target = 180;
-
+    private Integer[] A = {3, 2, 1, 0};
     public void run() {
 
-        Integer prevIndex = 0;
-        Integer thisIndex = 0;
-        Integer power = 1;
-        while (thisIndex < A.length && A[thisIndex] < target) {
-            prevIndex = thisIndex;
-            thisIndex += (int) Math.pow(2, power);
-            ++power;
-        }
-
-        if (thisIndex > A.length) {
-            thisIndex = A.length-1;
-        }
-
-        System.out.print(bs(prevIndex, thisIndex, target));
-
-    }
-
-    private String bs(Integer start, Integer end, Integer target) {
-        while (start <= end) {
-            Integer mid = Math.min(start, end) + ((Math.abs(start-end)))/2;
-
-            if (A[mid].equals( target)) {
-                return "found at " + mid;
-            } else if (A[mid] < target) {
-                start = mid+1;
-            } else if (A[mid] > target) {
-                end = mid-1;
+        Integer[] res = new Integer[A.length];
+        for (Integer i=0; i<A.length; ++i) {
+            if (A[i] >= 0 && A[i] < A.length) {
+                Integer j=A[i];
+                res[j] = i;
             }
         }
 
-        return "not found in search";
+        for (Integer i=0; i<res.length; ++i) {
+            if (res[i] != null) {
+                System.out.print(res[i] +" ");
+            } else {
+                System.out.print(A[i] +" ");
+            }
+        }
+
     }
 
 }
