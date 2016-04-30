9f58a9abfd3e1092ef8f99fd56e8c4e103d2ff84
shanxS <shanx.shashank@gmail.com>
2016-04-30 20:00:39 +0530

    kth max using sorting wiht randomness

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,142 +1,58 @@
+import java.util.Random;
+
 public class BinaryCode {
 
 
     private Integer[] A = {7, 10, 4, 3, 20, 15};
-    private Integer k = 5;
+    private Integer k = 6;
 
     public void run() {
 
-        BST bst = new BST(k);
-        for (Integer ele : A) {
-            bst.insert(ele);
-            System.out.println(bst.getMax());
-        }
+        sort (0, A.length-1);
+//        for (Integer ele : A) {
+//            System.out.print(ele + " ");
+//        }
 
-        System.out.print(bst.getMax());
-    }
-}
-
-class BST {
-    private Node head, maxParent;
-    private Integer size;
-
-    public BST(Integer size) {
-        this.size = size;
     }
 
-    public void insert(Integer value) {
-        if (head == null) {
-            head = new Node(value);
-        } else {
-            if (getMax() < value && size == 0) {
-                return;
-            }
-
-            insert(head, value);
-        }
-
-        updateMaxParent(head);
-
-        --size;
-        if (size < 0) {
-            removeMax();
-            updateMaxParent(head);
-            ++size;
-        }
-    }
-
-    private void removeMax() {
-        if (maxParent == null) {
-            head = head.getLeft();
-        } else {
-            maxParent.setRight(maxParent.getRight().getLeft());
-        }
-    }
-
-    private void updateMaxParent(Node node) {
-        maxParent = null;
-        while (node != null && node.getRight() != null) {
-            maxParent = node;
-            node = node.getRight();
-        }
-    }
-
-    private void insert(Node node, Integer value) {
-        if (node.getValue() > value) {
-            if (node.getLeft() == null) {
-                node.setLeft(new Node(value));
-            } else {
-                insert(node.getLeft(), value);
-            }
-        } else if (node.getValue() < value) {
-            if (node.getRight() == null) {
-                node.setRight(new Node(value));
-            } else {
-                insert(node.getRight(), value);
-            }
-        } else {
-            System.out.print("duplicate values " + value);
-        }
-    }
-
-    public Integer getMax() {
-        if (maxParent == null) {
-            return head.getValue();
-        } else {
-            return maxParent.getRight().getValue();
-        }
-    }
-
-
-    public void print() {
-        print(head);
-    }
-
-    private void print(Node node) {
-        if (node == null) {
+    private void sort(Integer start, Integer end) {
+        if (start > end) {
             return;
         }
 
-        System.out.print(node.getValue() + " - ");
-        if (node.getLeft() != null) {
-            System.out.print(node.getLeft().getValue());
+        Integer count = end - start + 1;
+        Random rand  = new Random();
+        swap(end, start + rand.nextInt(count));
+
+        Integer pivot = qSort(start, end);
+        if (pivot == k-1) {
+            System.out.print(A[pivot]);
+            return;
         }
-        System.out.print(", ");
-        if (node.getRight() != null) {
-            System.out.print(node.getRight().getValue());
+
+        sort(start, pivot-1);
+        sort(pivot+1, end);
+    }
+
+    private Integer qSort(Integer start, Integer end) {
+
+        Integer marker = start-1;
+        Integer pivotValue = A[end];
+
+        for (Integer i=start; i<end; ++i) {
+            if (A[i] <= pivotValue) {
+                ++marker;
+                swap(marker, i);
+            }
         }
-        System.out.println();
+        swap(marker+1, end);
 
-        print(node.getLeft());
-        print(node.getRight());
-    }
-}
-
-class Node {
-    private Node left, right;
-    private Integer value;
-
-    public Node(Integer value) {
-        this.value = value;
+        return marker+1;
     }
 
-    public Node getLeft() {
-        return left;
-    }
-
-    public Node getRight() {
-        return right;
-    }
-
-    public Integer getValue() {
-        return value;
-    }
-
-    public void setLeft(Node left) {
-        this.left = left;
-    }
-
-    public void setRight(Node right) {
-        this.right = right;
+    private void swap(Integer from, Integer to) {
+        Integer tmp = A[from];
+        A[from] = A[to];
+        A[to] = tmp;
     }
 }
