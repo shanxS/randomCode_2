98c32194818b58f8e76852b3edf6bcbeb2613868
shanxS <shanx.shashank@gmail.com>
2016-05-01 18:35:28 +0530

    Count all distinct pairs with difference equal to k

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,37 +1,59 @@
-import java.util.HashSet;
-
 public class BinaryCode {
 
-    private Integer[] A = {4, 2, 0, 1, 6};
-    private HashSet<Integer> set;
+    private Integer[] A = {1, 5, 3, 4, 2};
+    private Integer k=3;
 
     public void run() {
 
-        set = new HashSet<>();
-        Integer sumSoFar = 0;
+        sort(0, A.length-1);
 
-        for (Integer i=0; i<A.length; ++i) {
-            Integer ele = A[i];
+        Integer start = 0, end = A.length-1;
+        while (start < end) {
 
-            sumSoFar += ele;
-            if (set.contains(sumSoFar)) {
-                searchFrom(i);
-                break;
+            Integer diff = A[end]-A[start];
+
+            if (diff == k) {
+                System.out.println(A[end] + " " + A[start]);
+
+                --end;
+                ++start;
+            } else if (diff < k) {
+                --end;
             } else {
-                set.add(sumSoFar);
+                ++start;
             }
         }
 
     }
 
-    private void searchFrom(Integer index) {
-        Integer end = index;
-        Integer sum = A[index];
-        while (sum != 0 && index >= 0) {
-            --index;
-            sum += A[index];
+    private void sort(Integer start, Integer end) {
+        if (start >= end) {
+            return;
         }
 
-        System.out.print(index + " " + end);
+        Integer pivot = qSort(start, end);
+        sort(start, pivot-1);
+        sort(pivot+1, end);
+    }
+
+    private Integer qSort(Integer start, Integer end) {
+        Integer marker = start-1;
+        Integer pivotValue = A[end];
+
+        for (Integer i=start; i<end; ++i) {
+            if (A[i] <= pivotValue) {
+                ++marker;
+                swap(i, marker);
+            }
+        }
+
+        swap(marker+1, end);
+        return marker+1;
+    }
+
+    private void swap(Integer from, Integer to) {
+        Integer tmp = A[from];
+        A[from] = A[to];
+        A[to] = tmp;
     }
 }
