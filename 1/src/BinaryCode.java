0d8ba453b98effece488a1ad8ee1d8289167e215
shanxS <shanx.shashank@gmail.com>
2016-05-01 16:42:29 +0530

    Sort an array according to the order defined by another array - using sorting and bs

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,174 +1,79 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class BinaryCode {
 
     private Integer[] A1 = {2, 1, 2, 5, 7, 1, 9, 3, 6, 8, 8};
     private Integer[] A2 = {2, 1, 8, 3};
+    private Boolean[] visited = new Boolean[A1.length];
 
     public void run() {
-        BST bst = new BST();
-        for (Integer ele : A1) {
-            bst.insert(ele);
-        }
-
+        sort(0, A1.length-1);
         for (Integer ele : A2) {
-            Integer count = bst.getCount(ele);
-            while (count > 0) {
+            Integer index = find (ele);
+            while (index != null
+                    && index < A1.length
+                    && A1[index] == ele) {
                 System.out.print(ele + " ");
-                --count;
+                visited[index] = true;
+                ++index;
             }
         }
 
-        for (Integer ele : bst.getUnvisited()) {
-            System.out.print(ele + " ");
+        for (Integer i=0; i<A1.length; ++i) {
+            if (visited[i] ==null || !visited[i]) {
+                System.out.print(A1[i] + " ");
+            }
         }
 
     }
-}
 
-class BST {
-    private Node head;2 2 1 1 8 8 3 5 6 7 9
+    private Integer find(Integer ele) {
+        Integer start = 0;
+        Integer end = A1.length-1;
 
-    public void insert(Integer value) {
-        if (head == null) {
-            head = new Node(value);
-        } else {
-            insert(head, value);
-        }
-    }
+        while (start <= end) {
+            Integer mid = Math.min(start, end) + ((Math.abs(start-end))/2);
 
-    private void insert(Node node, Integer value) {
-        if (node.getValue() < value) {
-            if (node.getRight() == null) {
-                node.setRight(new Node(value));
+            if (A1[mid] == ele) {
+                return mid;
+            } else if (A1[mid] > ele) {
+                end = mid-1;
             } else {
-                insert(node.getRight(), value);
+                start = mid+1;
             }
-        } else if (node.getValue() > value) {
-            if (node.getLeft() == null) {
-                node.setLeft(new Node(value));
-            } else {
-                insert(node.getLeft(), value);
-            }
-        } else {
-            node.bumpCount();
         }
+
+
+        return null;
     }
 
-    public void print() {
-        print(head);
-    }
-
-    private void print(Node node) {
-        if (node == null) {
+    private void sort(Integer start, Integer end) {
+        if (start >= end) {
             return;
         }
 
-        System.out.print(node.getValue() + "(" + node.getCount() + ")" + " - ");
-        if (node.getLeft() != null) {
-            System.out.print(node.getLeft().getValue());
-        }
-        System.out.print(", ");
-        if (node.getRight() != null) {
-            System.out.print(node.getRight().getValue());
-        }
-        System.out.println();
-
-        print(node.getLeft());
-        print(node.getRight());
+        Integer pivot = qsort(start, end);
+        sort(start, pivot-1);
+        sort(pivot+1, end);
     }
 
-    public Integer getCount(Integer ele) {
-        Node node = findNode(head, ele);
-        if (node == null) {
-            return 0;
-        } else {
-            return node.getCount();
-        }
-    }
+    private Integer qsort(Integer start, Integer end) {
+        Integer marker = start-1;
+        Integer pivotValue = A1[end];
 
-    private Node findNode(Node node, Integer ele) {
-        if (node == null) {
-            return null;
+        for (Integer i=start; i<end; ++i) {
+            if (A1[i] < pivotValue) {
+                ++marker;
+                swap(marker, i);
+            }
         }
 
-        if (node.getValue() == ele) {
-            node.setVisited();
-            return node;
-        } else if (node.getValue() > ele) {
-            return findNode(node.getLeft(), ele);
-        } else {
-            return findNode(node.getRight(), ele);
-        }
+        swap(marker+1, end);
+        return marker+1;
     }
 
-    public List<Integer> getUnvisited() {
-        List<Integer> list = new ArrayList<>();
+    private void swap(Integer from, Integer to) {
+        Integer tmp = A1[from];
+        A1[from] = A1[to];
+        A1[to] = tmp;
 
-        collectUnvisited(head, list);
-
-        return list;
-    }
-
-    private void collectUnvisited(Node node, List<Integer> list) {
-        if (node == null) {
-            return;
-        }
-
-        collectUnvisited(node.getLeft(), list);
-        if (!node.getVisited()) {
-            list.add(node.getValue());
-        }
-        collectUnvisited(node.getRight(), list);
-    }
-}
-
-class Node {
-    private Node left, right;
-    private Boolean visited;
-    private Integer value;
-    private Integer count;
-
-    public Node(Integer v) {
-        this.value = v;
-        this.count = 1;
-        this.visited = false;
-    }
-
-    public Boolean getVisited() {
-        return visited;
-    }
-
-    public void setVisited() {
-        this.visited = true;
-    }
-
-    public void bumpCount() {
-        ++count;
-    }
-
-    public Integer getCount() {
-        return count;
-    }
-
-    public void setLeft(Node left) {
-        this.left = left;
-    }
-
-    public void setRight(Node right) {
-        this.right = right;
-    }
-
-    public Node getLeft() {
-        return left;
-    }
-
-    public Node getRight() {
-        return right;
-    }
-
-    public Integer getValue() {
-        return value;
     }
 }
