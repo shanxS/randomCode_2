76309b09b1063cb7ba7a6feb20119e8ac8ef32a0
shanxS <shanx.shashank@gmail.com>
2016-05-01 17:18:12 +0530

    Sort an array according to the order defined by another array - using hashing

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,79 +1,41 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class BinaryCode {
 
     private Integer[] A1 = {2, 1, 2, 5, 7, 1, 9, 3, 6, 8, 8};
     private Integer[] A2 = {2, 1, 8, 3};
-    private Boolean[] visited = new Boolean[A1.length];
+    private Map<Integer, Integer> numCounter;
 
     public void run() {
-        sort(0, A1.length-1);
+        numCounter = new TreeMap<>();
+        for (Integer ele : A1) {
+            Integer count = numCounter.get(ele);
+            if (count == null) {
+                count=0;
+            }
+            ++count;
+            numCounter.put(ele, count);
+        }
+
         for (Integer ele : A2) {
-            Integer index = find (ele);
-            while (index != null
-                    && index < A1.length
-                    && A1[index] == ele) {
+            Integer count = numCounter.get(ele);
+            if (count != null) {
+                numCounter.remove(ele);
+                while (count > 0) {
+                    System.out.print(ele + " ");
+                    --count;
+                }
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : numCounter.entrySet()) {
+            Integer ele = entry.getKey();
+            Integer count = entry.getValue();
+            while (count > 0) {
                 System.out.print(ele + " ");
-                visited[index] = true;
-                ++index;
+                --count;
             }
         }
-
-        for (Integer i=0; i<A1.length; ++i) {
-            if (visited[i] ==null || !visited[i]) {
-                System.out.print(A1[i] + " ");
-            }
-        }
-
-    }
-
-    private Integer find(Integer ele) {
-        Integer start = 0;
-        Integer end = A1.length-1;
-
-        while (start <= end) {
-            Integer mid = Math.min(start, end) + ((Math.abs(start-end))/2);
-
-            if (A1[mid] == ele) {
-                return mid;
-            } else if (A1[mid] > ele) {
-                end = mid-1;
-            } else {
-                start = mid+1;
-            }
-        }
-
-
-        return null;
-    }
-
-    private void sort(Integer start, Integer end) {
-        if (start >= end) {
-            return;
-        }
-
-        Integer pivot = qsort(start, end);
-        sort(start, pivot-1);
-        sort(pivot+1, end);
-    }
-
-    private Integer qsort(Integer start, Integer end) {
-        Integer marker = start-1;
-        Integer pivotValue = A1[end];
-
-        for (Integer i=start; i<end; ++i) {
-            if (A1[i] < pivotValue) {
-                ++marker;
-                swap(marker, i);
-            }
-        }
-
-        swap(marker+1, end);
-        return marker+1;
-    }
-
-    private void swap(Integer from, Integer to) {
-        Integer tmp = A1[from];
-        A1[from] = A1[to];
-        A1[to] = tmp;
-
     }
 }
