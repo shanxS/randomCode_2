5e129575604ca963aeb8e4352ea685e46cec9869
shanxS <shanx.shashank@gmail.com>
2016-05-01 21:44:32 +0530

    Merge k sorted arrays

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,27 +1,69 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class BinaryCode {
 
-    private Integer[] A = {1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0};
+    private List<Integer[]> arrays;
+    private Map<Integer, Integer> arrayIndex;
 
     public void run() {
-        Integer w = null, r = 0;
+        init();
 
-        while (r < A.length) {
+        List<Integer> list = new ArrayList<>();
+        while (true) {
 
-            if (A[r] == 0 && w == null) {
-                w = r;
-            } else if (A[r] != 0 && w != null) {
-                A[w] = A[r];
-                A[r] = 0;
-                ++w;
+            Integer min = findMin();
+            if (min == null) {
+                break;
+            } else {
+                list.add(min);
             }
 
-            ++r;
+        }
+
+        System.out.print(list);
+
+    }
+
+    private Integer findMin() {
+
+        Integer min = null;
+        Integer minArray = null;
+
+        for (Map.Entry<Integer, Integer> entry : arrayIndex.entrySet()) {
+
+            Integer array = entry.getKey();
+            Integer index = entry.getValue();
+            if (index < arrays.get(array).length) {
+
+                if (min == null || min > arrays.get(array)[index]) {
+                    min = arrays.get(array)[index];
+                    minArray = array;
+                }
+
+            }
 
         }
 
-        for (Integer ele : A) {
-            System.out.print(ele + " ");
+        if (min != null) {
+            arrayIndex.put(minArray, arrayIndex.get(minArray) + 1);
         }
 
+
+        return min;
+    }
+
+    private void init() {
+        arrays = new ArrayList<>();
+        arrays.add(new Integer[]{2, 6, 12, 34});
+        arrays.add(new Integer[]{1, 9, 20, 1000});
+        arrays.add(new Integer[]{23, 34, 90, 2000});
+
+        arrayIndex = new HashMap<>();
+        arrayIndex.put(0, 0);
+        arrayIndex.put(1, 0);
+        arrayIndex.put(2, 0);
     }
 }
