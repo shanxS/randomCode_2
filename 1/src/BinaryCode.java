3be33435d0a4261b2662e2e5298c31492a5f5074
shanxS <shanx.shashank@gmail.com>
2016-05-03 18:54:10 +0530

    radix sort

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,33 +1,61 @@
 public class BinaryCode {
 
-    private int[] A = {1,1,4,2,5,3,9,5};
+    private int[] A = {170, 450, 75, 90, 802, 24, 2, 66};
     private Integer pass;
 
     public void run() {
+
+        initPass();
+
+        Integer thisPass = 0;
+        while (thisPass < pass) {
+            int[] sorted = sortFor(thisPass);
+            A = sorted;
+            ++thisPass;
+        }
+
+        for (Integer ele : A) {
+            System.out.print(ele + " ");
+        }
+
+    }
+
+    private int[] sortFor(Integer thisPass) {
         int[] count = new int[10];
         for (Integer i=0; i<A.length; ++i) {
-            count[A[i]] += 1;
+            ++count[getIndex(A[i], thisPass)];
         }
 
         for (Integer i=1; i<count.length; ++i) {
             count[i] += count[i-1];
         }
 
-        for (Integer i : count) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
         int[] sorted = new int[A.length];
-        for (Integer i=0; i<A.length; ++i) {
-            sorted[count[A[i]] - 1] = A[i];
-            --count[A[i]];
+        for (Integer i=A.length-1; i>=0; --i) {
+            sorted[count[getIndex(A[i], thisPass)] - 1] = A[i];
+            --count[getIndex(A[i], thisPass)];
         }
 
-        for (Integer ele : sorted) {
-            System.out.print(ele + " ");
+
+        return sorted;
+    }
+
+    private int getIndex(int value, Integer pass) {
+        value /= (int) Math.pow(10, pass);
+        return value % 10;
+    }
+
+    public void initPass() {
+        Integer max = Integer.MIN_VALUE;
+        for (Integer ele : A) {
+            max = Math.max(ele, max);
         }
 
+        pass = 0;
+        while (max != 0) {
+            max /= 10;
+            ++pass;
+        }
     }
 
 }
