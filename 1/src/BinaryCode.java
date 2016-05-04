8c7b1a5a6f0d718838ec41ad4f4f70188af4ee42
shanxS <shanx.shashank@gmail.com>
2016-05-04 22:11:22 +0530

    count islands- practice

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -14,16 +14,18 @@ public class BinaryCode {
     public void run() {
 
         Integer count = 0;
+
         for (Integer r=0; r<A.length; ++r) {
             for (Integer c=0; c<A[0].length; ++c) {
+
                 if (A[r][c] == 1 && !visited[r][c]) {
-                    count += countFor(r,c);
+                    count += countFor(r, c);
                 }
+
             }
         }
 
         System.out.print(count);
-
     }
 
     private Integer countFor(Integer r, Integer c) {
@@ -33,85 +35,85 @@ public class BinaryCode {
         cStack.push(c);
 
 
-        while (rStack.size() > 0 && cStack.size() > 0) {
-            up(r, c, rStack, cStack);
-            down(r, c, rStack, cStack);
-            left(r, c, rStack, cStack);
-            right(r, c, rStack, cStack);
+        while (rStack.size()>0 && cStack.size()>0) {
 
-            leftUp(r, c, rStack, cStack);
-            leftDown(r, c, rStack, cStack);
-            rightUp(r, c, rStack, cStack);
-            rightDown(r, c, rStack, cStack);
+            up(r,c,rStack,cStack);
+            down(r,c,rStack,cStack);
+            left(r,c,rStack,cStack);
+            right(r,c,rStack,cStack);
+
+            upLeft(r,c,rStack,cStack);
+            upRight(r,c,rStack,cStack);
+            downLeft(r,c,rStack,cStack);
+            downRight(r,c,rStack,cStack);
 
             visited[r][c] = true;
-            r=rStack.pop();
-            c=cStack.pop();
-
+            r = rStack.pop();
+            c = cStack.pop();
         }
 
-        return 1;
 
+        return 1;
     }
 
-    private void rightDown(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
-        if (isValid(r+1, c+1) && A[r+1][c+1] == 1) {
+    private void downRight(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
+        if (valid(r+1, c+1) && A[r+1][c+1] == 1) {
             rStack.push(r+1);
             cStack.push(c+1);
         }
     }
 
-    private boolean isValid(int r, int c) {
-        return (r >=0 && r<A.length)
-                && (c>=0 && c<A[0].length) && !visited[r][c];
-    }
-
-    private void rightUp(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
-        if (isValid(r-1, c+1) && A[r-1][c+1] == 1) {
-            rStack.push(r-1);
-            cStack.push(c+1);
-        }
-    }
-
-    private void leftDown(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
-        if (isValid(r+1, c-1) && A[r+1][c-1] == 1) {
+    private void downLeft(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
+        if (valid(r+1, c-1) && A[r+1][c-1] == 1) {
             rStack.push(r+1);
             cStack.push(c-1);
         }
     }
 
-    private void leftUp(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
-        if (isValid(r-1, c-1) && A[r-1][c-1] == 1) {
+    private void upRight(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
+        if (valid(r-1, c+1) && A[r-1][c+1] == 1) {
+            rStack.push(r-1);
+            cStack.push(c+1);
+        }
+    }
+
+    private void upLeft(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
+        if (valid(r-1, c-1) && A[r-1][c-1] == 1) {
             rStack.push(r-1);
             cStack.push(c-1);
         }
     }
 
     private void right(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
-        if (isValid(r, c+1) && A[r][c+1] == 1) {
+        if (valid(r, c+1) && A[r][c+1] == 1) {
             rStack.push(r);
             cStack.push(c+1);
         }
     }
 
     private void left(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
-        if (isValid(r, c-1) && A[r][c-1] == 1) {
+        if (valid(r, c-1) && A[r][c-1] == 1) {
             rStack.push(r);
             cStack.push(c-1);
         }
     }
 
     private void down(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
-        if (isValid(r+1, c) && A[r+1][c] == 1) {
+        if (valid(r+1, c) && A[r+1][c] == 1) {
             rStack.push(r+1);
             cStack.push(c);
         }
     }
 
+    private boolean valid(int r, Integer c) {
+        return (r>=0 && r<A.length) && (c>=0 && c<A[0].length) && !visited[r][c];
+    }
+
     private void up(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
-        if (isValid(r-1, c) && A[r-1][c] == 1) {
+        if (valid(r-1, c) && A[r-1][c] == 1) {
             rStack.push(r-1);
             cStack.push(c);
         }
     }
+
 }
