e236daf8df821fbe0f46eda9359df9207b9163e6
shanxS <shanx.shashank@gmail.com>
2016-05-05 18:41:58 +0530

    Find the Increasing subsequence of length three with maximum product

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,119 +1,115 @@
-import java.util.Stack;
-
 public class BinaryCode {
 
-    private Integer[][] A = {
-            {1, 1, 0, 0, 0},
-            {0, 1, 0, 0, 1},
-            {1, 0, 0, 1, 1},
-            {0, 0, 0, 0, 0},
-            {1, 0, 1, 0, 1}};
-
-    private boolean[][] visited = new boolean[A.length][A[0].length];
+    private Integer[] A = {6, 7, 8, 1, 2, 3, 9, 10};
+    Integer[] maxL = new Integer[A.length];
+    Integer[] maxR = new Integer[A.length];
 
     public void run() {
 
-        Integer count = 0;
+        BST bst = new BST();
+        for (Integer i=0; i<A.length; ++i) {
+            maxL[i] = bst.insert(A[i]);
+        }
 
-        for (Integer r=0; r<A.length; ++r) {
-            for (Integer c=0; c<A[0].length; ++c) {
-
-                if (A[r][c] == 1 && !visited[r][c]) {
-                    count += countFor(r, c);
-                }
-
+        for (Integer i=A.length-2; i>=0; --i) {
+            if (i+1 == A.length-1) {
+                maxR[i] = A[i+1];
+            } else {
+                maxR[i] = Math.max(maxR[i+1], A[i+1]);
             }
         }
 
-        System.out.print(count);
-    }
+        Integer[] values = new Integer[3];
+        Integer maxProd = Integer.MIN_VALUE;
+        for (Integer i=1; i<A.length-1; ++i) {
+            Integer biggestLeft = maxL[i];
+            Integer biggestRight = (maxR[i] < A[i]) ? null : maxR[i];
 
-    private Integer countFor(Integer r, Integer c) {
-        Stack<Integer> rStack = new Stack<>();
-        rStack.push(r);
-        Stack<Integer> cStack = new Stack<>();
-        cStack.push(c);
+            if (biggestLeft != null
+                    && biggestRight != null
+                    && maxProd < (biggestLeft * biggestRight * A[i])) {
+                maxProd = (biggestLeft * biggestRight * A[i]);
+                values[0] = biggestLeft;
+                values[1] = A[i];
+                values[2] = biggestRight;
+            }
 
-
-        while (rStack.size()>0 && cStack.size()>0) {
-
-            up(r,c,rStack,cStack);
-            down(r,c,rStack,cStack);
-            left(r,c,rStack,cStack);
-            right(r,c,rStack,cStack);
-
-            upLeft(r,c,rStack,cStack);
-            upRight(r,c,rStack,cStack);
-            downLeft(r,c,rStack,cStack);
-            downRight(r,c,rStack,cStack);
-
-            visited[r][c] = true;
-            r = rStack.pop();
-            c = cStack.pop();
         }
 
-
-        return 1;
-    }
-
-    private void downRight(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
-        if (valid(r+1, c+1) && A[r+1][c+1] == 1) {
-            rStack.push(r+1);
-            cStack.push(c+1);
+        System.out.println(maxProd);
+        for (Integer ele : values) {
+            System.out.print(ele + " ");
         }
     }
+}
 
-    private void downLeft(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
-        if (valid(r+1, c-1) && A[r+1][c-1] == 1) {
-            rStack.push(r+1);
-            cStack.push(c-1);
+class BST {
+    private Node head;
+
+    public Integer insert(Integer value) {
+        Node node = new Node(value);
+
+        if (head == null) {
+            head = node;
+        } else {
+            insert(head, node);
+        }
+
+        return node.getBiggestSmallValue();
+    }
+
+    private void insert(Node parent, Node child) {
+        if (parent.getValue() > child.getValue()) {
+            if (parent.getLeft() == null) {
+                parent.setLeft(child);
+            } else {
+                insert(parent.getLeft(), child);
+            }
+        } else if (parent.getValue() < child.getValue()) {
+            child.setBiggestSmallValue(parent.getValue());
+
+            if (parent.getRight() == null) {
+                parent.setRight(child);
+            } else {
+                insert(parent.getRight(), child);
+            }
         }
     }
+}
 
-    private void upRight(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
-        if (valid(r-1, c+1) && A[r-1][c+1] == 1) {
-            rStack.push(r-1);
-            cStack.push(c+1);
-        }
+class Node {
+    private Node left, right;
+    private Integer value, biggestSmallValue;
+
+    public Integer getBiggestSmallValue() {
+        return biggestSmallValue;
     }
 
-    private void upLeft(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
-        if (valid(r-1, c-1) && A[r-1][c-1] == 1) {
-            rStack.push(r-1);
-            cStack.push(c-1);
-        }
+    public void setBiggestSmallValue(Integer biggestSmallValue) {
+        this.biggestSmallValue = biggestSmallValue;
     }
 
-    private void right(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
-        if (valid(r, c+1) && A[r][c+1] == 1) {
-            rStack.push(r);
-            cStack.push(c+1);
-        }
+    public Node(Integer value) {
+        this.value = value;
     }
 
-    private void left(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
-        if (valid(r, c-1) && A[r][c-1] == 1) {
-            rStack.push(r);
-            cStack.push(c-1);
-        }
+    public void setLeft(Node left) {
+        this.left = left;
     }
 
-    private void down(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
-        if (valid(r+1, c) && A[r+1][c] == 1) {
-            rStack.push(r+1);
-            cStack.push(c);
-        }
+    public void setRight(Node right) {
+        this.right = right;
     }
 
-    private boolean valid(int r, Integer c) {
-        return (r>=0 && r<A.length) && (c>=0 && c<A[0].length) && !visited[r][c];
+    public Node getLeft() {
+        return left;
     }
 
-    private void up(Integer r, Integer c, Stack<Integer> rStack, Stack<Integer> cStack) {
-        if (valid(r-1, c) && A[r-1][c] == 1) {
-            rStack.push(r-1);
-            cStack.push(c);
-        }
+    public Node getRight() {
+        return right;
     }
 
+    public Integer getValue() {
+        return value;
+    }
 }
