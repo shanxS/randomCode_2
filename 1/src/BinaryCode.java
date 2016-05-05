197afbac0147fc92ef35b66bc1b2e4d5d3882528
shanxS <shanx.shashank@gmail.com>
2016-05-05 21:09:25 +0530

    Given an array of of size n and a number k, find all elements that appear more than n/k times - using stupid counter

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -2,57 +2,91 @@ public class BinaryCode {
 
     private Integer[] A = {3,1,2,2,2,1,4,3,3};
     private Integer k = 4;
+    private Holder[] holder = new Holder[k-1];
 
     public void run() {
 
-        sort(0, A.length-1);
-
-        Integer counter = 0;
-        while (counter < A.length) {
-
-            Integer thisValue = A[counter];
-            Integer valueCount = 1;
-            ++counter;
-            while (counter < A.length && A[counter] == thisValue) {
-                ++valueCount;
-                ++counter;
-            }
-
-            if (valueCount > (A.length/k)) {
-                System.out.println(thisValue);
-            }
-
-        }
-    }
-
-    private void sort(int start, int end) {
-        if (start >= end) {
-            return;
-        }
-
-        Integer pivot = qSort(start, end);
-        sort(start, pivot-1);
-        sort(pivot+1, end);
-    }
-
-    private Integer qSort(int start, int end) {
-        Integer marker = start-1;
-        Integer pivotValue = A[end];
-
-        for (Integer i=start; i<end; ++i) {
-            if (A[i] <= pivotValue) {
-                ++marker;
-                swap(marker, i);
+        for (Integer i=0; i<A.length; ++i) {
+            Holder thisHolder = findHolderFor(A[i]);
+            if (thisHolder == null) {
+                knockOffMinHolder();
+                insertHolder(A[i]);
+            } else {
+                thisHolder.count++;
             }
         }
 
-        swap(marker+1, end);
-        return marker+1;
+        for (Integer i=0; i<holder.length; ++i) {
+            if (holder[i] != null) {
+                Integer value = holder[i].value;
+
+                Integer count = 0;
+                for (Integer j=0; j<A.length; ++j) {
+                    if (A[j] == value) {
+                        ++count;
+                    }
+                }
+                if (count > (A.length/k)) {
+                    System.out.print(value + " ");
+                }
+            }
+        }
+
     }
 
-    private void swap(int from, int to) {
-        Integer tmp = A[from];
-        A[from] = A[to];
-        A[to] = tmp;
+    private void insertHolder(Integer value) {
+
+        for (Integer i=0; i<holder.length; ++i) {
+            if (holder[i] == null) {
+                holder[i] = new Holder(value);
+                return;
+            }
+        }
+
+    }
+
+    private void knockOffMinHolder() {
+        Holder minHolder = null;
+        Integer index = null;
+        for (Integer i=0; i<holder.length; ++i) {
+            if (holder[i] != null) {
+                if (minHolder == null) {
+                    minHolder = holder[i];
+                    index = i;
+                } else {
+                    minHolder = (minHolder.count < holder[i].count) ? minHolder : holder[i];
+                    index = i;
+                }
+            }
+        }
+
+        if (index != null) {
+            if (minHolder.count == 1) {
+                holder[index] = null;
+            } else {
+                --minHolder.count;
+            }
+        }
+
+    }
+
+    private Holder findHolderFor(Integer value) {
+        for (Integer i=0; i<holder.length; ++i) {
+            if (holder[i] != null && holder[i].value == value) {
+                return holder[i];
+            }
+        }
+
+        return null;
+    }
+}
+
+class Holder {
+    public Integer value;
+    public Integer count;
+
+    public Holder(Integer value) {
+        this.value = value;
+        this.count = 1;
     }
 }
