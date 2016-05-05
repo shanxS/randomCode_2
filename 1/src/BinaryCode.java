84ffd08f40a1a3d16758b050f58bb1256d373951
shanxS <shanx.shashank@gmail.com>
2016-05-05 20:47:12 +0530

    Given an array of of size n and a number k, find all elements that appear more than n/k times - using sorting

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,53 +1,58 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class BinaryCode {
 
-    private Integer[] A = {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
-    private List<List<Integer>> store;
+    private Integer[] A = {3,1,2,2,2,1,4,3,3};
+    private Integer k = 4;
 
     public void run() {
-        store = new ArrayList<>();
-        List<Integer> data = new ArrayList<>();
 
-        for (Integer i=0; i<A.length; ++i) {
+        sort(0, A.length-1);
 
-            if (i==0) {
-                data.add(A[i]);
-            } else if (data.get(data.size()-1) < A[i]) {
-                data.add(A[i]);
-                updateStored (A[i]);
-            } else {
-                save (data);
-                replace(data, A[i]);
+        Integer counter = 0;
+        while (counter < A.length) {
+
+            Integer thisValue = A[counter];
+            Integer valueCount = 1;
+            ++counter;
+            while (counter < A.length && A[counter] == thisValue) {
+                ++valueCount;
+                ++counter;
+            }
+
+            if (valueCount > (A.length/k)) {
+                System.out.println(thisValue);
             }
 
         }
-
-        System.out.print(data.size() + " --- " + data + " ---- " + store);
-
     }
 
-    private void save(List<Integer> data) {
-        store.add(new ArrayList<>(data));
+    private void sort(int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        Integer pivot = qSort(start, end);
+        sort(start, pivot-1);
+        sort(pivot+1, end);
     }
 
-    private void updateStored(Integer value) {
+    private Integer qSort(int start, int end) {
+        Integer marker = start-1;
+        Integer pivotValue = A[end];
 
-        for (List<Integer> list : store) {
-            if (list.get(list.size()-1) < value) {
-                list.add(value);
+        for (Integer i=start; i<end; ++i) {
+            if (A[i] <= pivotValue) {
+                ++marker;
+                swap(marker, i);
             }
         }
 
+        swap(marker+1, end);
+        return marker+1;
     }
 
-    private void replace(List<Integer> data, Integer value) {
-        Integer counter = data.size()-1;
-        while (counter >=0 && data.get(counter) > value) {
-            --counter;
-        }
-
-        data.set(counter+1, value);
+    private void swap(int from, int to) {
+        Integer tmp = A[from];
+        A[from] = A[to];
+        A[to] = tmp;
     }
 }
