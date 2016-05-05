259d43b219ec8e2fc4f1fbbffa4b1ad15686354a
shanxS <shanx.shashank@gmail.com>
2016-05-05 19:51:19 +0530

    LIS - get list as well

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -4,8 +4,10 @@ import java.util.List;
 public class BinaryCode {
 
     private Integer[] A = {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
+    private List<List<Integer>> store;
 
     public void run() {
+        store = new ArrayList<>();
         List<Integer> data = new ArrayList<>();
 
         for (Integer i=0; i<A.length; ++i) {
@@ -14,13 +16,30 @@ public class BinaryCode {
                 data.add(A[i]);
             } else if (data.get(data.size()-1) < A[i]) {
                 data.add(A[i]);
+                updateStored (A[i]);
             } else {
+                save (data);
                 replace(data, A[i]);
             }
 
         }
 
-        System.out.print(data.size() + " --- " + data);
+        System.out.print(data.size() + " --- " + data + " ---- " + store);
+
+    }
+
+    private void save(List<Integer> data) {
+        store.add(new ArrayList<>(data));
+    }
+
+    private void updateStored(Integer value) {
+
+        for (List<Integer> list : store) {
+            if (list.get(list.size()-1) < value) {
+                list.add(value);
+            }
+        }
+
     }
 
     private void replace(List<Integer> data, Integer value) {
