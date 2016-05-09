50258cc7a616ae151513ac8ac0b65eafab538bd4
shanxS <shanx.shashank@gmail.com>
2016-05-09 19:03:33 +0530

    sort by freq using hashmap

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,15 +1,64 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class BinaryCode {
 
-    private Integer[] A = {100,10,5,15,10};
+    private Integer[] A = {2, 5, 2, 8, 5, 7, 6, 8, 8};
+    private Map<Integer, Integer> numberPos, numberFreq;
+    private Map<Integer, List<Integer>> freqNumbers;
 
     public void run() {
+
+        numberPos = new HashMap<>();
+        numberFreq = new HashMap<>();
+        freqNumbers = new HashMap<>();
+
         for (Integer i=0; i<A.length; ++i) {
-            if ((i==0 && A[i] > A[i+1])
-                    || (i==A.length-1 && A[i] > A[i-1])
-                    || (i>0 && i<A.length-1 && A[i] > A[i-1] && A[i] > A[i+1])) {
-                System.out.print(A[i]);
-                break;
+            Integer num = A[i];
+
+            if (numberPos.get(num) == null) {
+                numberPos.put(num, i);
+            }
+
+            Integer freq = numberFreq.get(num);
+            if (freq == null) {
+                freq = 0;
+            }
+            ++freq;
+            numberFreq.put(num, freq);
+
+            List<Integer> prevNumbers = freqNumbers.get(freq-1);
+            if (prevNumbers != null) {
+                prevNumbers.remove(num);
+            }
+
+            List<Integer> numbers = freqNumbers.get(freq);
+            if (numbers == null) {
+                numbers = new ArrayList<>();
+                freqNumbers.put(freq, numbers);
+            }
+            numbers.add(num);
+
+        }
+
+        Integer[] sorted = new Integer[A.length];
+        Integer marker = 0;
+        for (Map.Entry<Integer, List<Integer>> entry : freqNumbers.entrySet()) {
+            for (Integer num : entry.getValue()) {
+                Integer count = entry.getKey();
+                while (count > 0) {
+                    sorted[marker] = num;
+                    ++marker;
+                    --count;
+                }
             }
         }
+
+        for (Integer ele : sorted) {
+            System.out.print(ele + " ");
+        }
+
     }
 }
