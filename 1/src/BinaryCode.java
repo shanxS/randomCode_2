fa6d5769f6e904a97376d228c5b31c75c53d5794
shanxS <shanx.shashank@gmail.com>
2016-05-11 22:19:17 +0530

    Maximum Subarray Sum divide and conquer - practice

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,37 +1,43 @@
 public class BinaryCode {
 
-    private int[] A = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    private int[] A = {-2, -5, 6, -2, -3, 1, 5, -6};
 
     public void run() {
-
-        int gStart = -1;
-        int gEnd = -1;
-        int gMax = Integer.MIN_VALUE;
-
-        int start = 0;
-        int end = 0;
-        int max = A[0];
-        for (int i=1; i<A.length; ++i) {
-            if (A[i] > 0 && max <= 0) {
-                max = A[i];
-                start = i;
-                end = i;
-            } else if (max > 0) {
-
-                max += A[i];
-                end = i;
-
-            }
-
-            if (gMax < max) {
-                gMax = max;
-                gStart = start;
-                gEnd = end;
-            }
-        }
-
-        System.out.print(gStart + " " + gEnd + " " + gMax);
-
+        System.out.print(findMaxIn(0, A.length-1));
     }
 
+    private int findMaxIn(int start, int end) {
+        if (start > end) {
+            return Integer.MIN_VALUE;
+        }
+
+        if (start == end) {
+            return A[start];
+        }
+
+        int mid = Math.min(start, end) + ((Math.abs(start-end))/2);
+        int rightMax = findMaxIn(start, mid);
+        int leftMax = findMaxIn(mid+1, end);
+        int midMax = findMinMax(start, mid, end);
+
+        return Math.max(midMax, Math.max(rightMax, leftMax));
+    }
+
+    private int findMinMax(int start, int mid, int end) {
+        int sum = 0;
+        int leftSum = 0;
+        for (int i=mid; i>=start; --i) {
+            sum += A[i];
+            leftSum = Math.max(leftSum, sum);
+        }
+
+        sum = 0;
+        int rightSum = 0;
+        for (int i=mid+1; i<=end; ++i) {
+            sum += A[i];
+            rightSum = Math.max(sum, rightSum);
+        }
+
+        return leftSum + rightSum;
+    }
 }
