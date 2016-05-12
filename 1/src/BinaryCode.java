6cd8c5dab88667311b57eb36875e0a7ef3b65a7e
shanxS <shanx.shashank@gmail.com>
2016-05-12 20:53:49 +0530

    Tug of War

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,43 +1,74 @@
 public class BinaryCode {
 
-    private int[] A = {-2, -5, 6, -2, -3, 1, 5, -6};
+    private Integer[] A =  {23, 45, -34, 12, 0, 98, -99, 4, 189, -1, 4};//{3, 4, 5};//, -3, 100, 1, 89, 54, 23, 20};
+    private int targetSize = (A.length % 2 == 0) ? A.length/2 : (A.length-1)/2;
+    private int globalMin = Integer.MAX_VALUE;
+    private Integer globalWith, globalWithout;
 
     public void run() {
-        System.out.print(findMaxIn(0, A.length-1));
+
+
+
+        int with = A[0], without = 0;
+        int withSize = 1, withoutSize = 0;
+        findMin(1, with, withSize, without, withoutSize);
+        System.out.print(globalMin + " " + globalWith + " " + globalWithout);
+
     }
 
-    private int findMaxIn(int start, int end) {
-        if (start > end) {
-            return Integer.MIN_VALUE;
+    private void findMin(int thisIndex, int prevWith, int prevWithSize, int prevWithout, int prevWithoutSize) {
+
+        if (A.length % 2 == 0) {
+            if (prevWithSize == targetSize && prevWithoutSize == targetSize) {
+                if (globalMin > Math.abs(prevWith - prevWithout)) {
+                    globalMin = Math.abs(prevWith - prevWithout);
+                    globalWith = prevWith;
+                    globalWithout = prevWithout;
+                }
+
+                return;
+            }
+        } else if (A.length % 2 != 0) {
+
+            if ( prevWithSize > prevWithout
+                    && prevWithSize == targetSize+1
+                    && prevWithoutSize == targetSize) {
+                if (globalMin > Math.abs(prevWith - prevWithout)) {
+                    globalMin = Math.abs(prevWith - prevWithout);
+                    globalWith = prevWith;
+                    globalWithout = prevWithout;
+                }
+
+                return;
+            } else if ( prevWithSize < prevWithout
+                    && prevWithSize == targetSize
+                    && prevWithoutSize == targetSize+1) {
+                if (globalMin > Math.abs(prevWith - prevWithout)) {
+                    globalMin = Math.abs(prevWith - prevWithout);
+                    globalWith = prevWith;
+                    globalWithout = prevWithout;
+                }
+
+                return;
+            }
+
         }
 
-        if (start == end) {
-            return A[start];
+        if (thisIndex >= A.length) {
+            return;
         }
 
-        int mid = Math.min(start, end) + ((Math.abs(start-end))/2);
-        int rightMax = findMaxIn(start, mid);
-        int leftMax = findMaxIn(mid+1, end);
-        int midMax = findMinMax(start, mid, end);
+        // with
+        findMin(thisIndex+1,
+                prevWith+A[thisIndex], prevWithSize+1,
+                prevWithout, prevWithoutSize);
 
-        return Math.max(midMax, Math.max(rightMax, leftMax));
+        findMin(thisIndex+1,
+                prevWithout+A[thisIndex], prevWithoutSize+1,
+                prevWith, prevWithSize);
+
+
+
     }
 
-    private int findMinMax(int start, int mid, int end) {
-        int sum = 0;
-        int leftSum = 0;
-        for (int i=mid; i>=start; --i) {
-            sum += A[i];
-            leftSum = Math.max(leftSum, sum);
-        }
-
-        sum = 0;
-        int rightSum = 0;
-        for (int i=mid+1; i<=end; ++i) {
-            sum += A[i];
-            rightSum = Math.max(sum, rightSum);
-        }
-
-        return leftSum + rightSum;
-    }
 }
