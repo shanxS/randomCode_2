1771300e2a7b7fc483831e4dd727019370528235
shanxS <shanx.shashank@gmail.com>
2016-05-12 22:16:07 +0530

    raidx sort

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,25 +1,63 @@
-import java.util.Arrays;
-
 public class BinaryCode {
 
-    private Integer[] A = {2,4,1,3,6};
+    private int[] A = {170, 45, 75, 90, 802, 24, 2, 66};
 
     public void run() {
+        Integer passCount = getPassCount();
+
+        Integer thisPass = 0;
+        while (thisPass < passCount) {
+            A = sortFor(thisPass);
+            ++thisPass;
+        }
+
+        for (int ele : A) {
+            System.out.print(ele + " ");
+        }
+    }
+
+    private int[] sortFor(Integer thisPass) {
         int[] count = new int[10];
         for (int i=0; i<A.length; ++i) {
-            ++count[A[i]];
+            ++count[getNumberFor(A[i], thisPass)];
         }
 
         for (int i=1; i<count.length; ++i) {
             count[i] += count[i-1];
         }
 
-        Integer[] sorted = new Integer[A.length];
-        for (int i=0; i<A.length; ++i) {
-            sorted[count[A[i]] - 1] = A[i];
-            --count[A[i]];
+        int[] sort = new int[A.length];
+        for (int i=A.length-1; i>=0; --i) {
+            sort[count[getNumberFor(A[i], thisPass)] - 1] = A[i];
+            --count[getNumberFor(A[i], thisPass)];
         }
 
-        System.out.print(Arrays.deepToString(sorted));
+        return sort;
     }
+
+    private int getNumberFor(int num, int pass) {
+        while (pass > 0) {
+            num /= 10;
+            --pass;
+        }
+
+        return num%10;
+    }
+
+    private Integer getPassCount() {
+        Integer max = Integer.MIN_VALUE;
+        for (Integer ele : A) {
+            max = Math.max(max, ele);
+        }
+
+        int pass = 0;
+        while (max > 0) {
+            max /= 10;
+            ++pass;
+        }
+
+        return pass;
+    }
+
+
 }
