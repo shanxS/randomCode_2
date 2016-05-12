12e433790e9479cce3ac38af323ccdbfa6392e72
shanxS <shanx.shashank@gmail.com>
2016-05-12 22:03:05 +0530

    count sort

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,74 +1,25 @@
+import java.util.Arrays;
+
 public class BinaryCode {
 
-    private Integer[] A =  {23, 45, -34, 12, 0, 98, -99, 4, 189, -1, 4};//{3, 4, 5};//, -3, 100, 1, 89, 54, 23, 20};
-    private int targetSize = (A.length % 2 == 0) ? A.length/2 : (A.length-1)/2;
-    private int globalMin = Integer.MAX_VALUE;
-    private Integer globalWith, globalWithout;
+    private Integer[] A = {2,4,1,3,6};
 
     public void run() {
-
-
-
-        int with = A[0], without = 0;
-        int withSize = 1, withoutSize = 0;
-        findMin(1, with, withSize, without, withoutSize);
-        System.out.print(globalMin + " " + globalWith + " " + globalWithout);
-
-    }
-
-    private void findMin(int thisIndex, int prevWith, int prevWithSize, int prevWithout, int prevWithoutSize) {
-
-        if (A.length % 2 == 0) {
-            if (prevWithSize == targetSize && prevWithoutSize == targetSize) {
-                if (globalMin > Math.abs(prevWith - prevWithout)) {
-                    globalMin = Math.abs(prevWith - prevWithout);
-                    globalWith = prevWith;
-                    globalWithout = prevWithout;
-                }
-
-                return;
-            }
-        } else if (A.length % 2 != 0) {
-
-            if ( prevWithSize > prevWithout
-                    && prevWithSize == targetSize+1
-                    && prevWithoutSize == targetSize) {
-                if (globalMin > Math.abs(prevWith - prevWithout)) {
-                    globalMin = Math.abs(prevWith - prevWithout);
-                    globalWith = prevWith;
-                    globalWithout = prevWithout;
-                }
-
-                return;
-            } else if ( prevWithSize < prevWithout
-                    && prevWithSize == targetSize
-                    && prevWithoutSize == targetSize+1) {
-                if (globalMin > Math.abs(prevWith - prevWithout)) {
-                    globalMin = Math.abs(prevWith - prevWithout);
-                    globalWith = prevWith;
-                    globalWithout = prevWithout;
-                }
-
-                return;
-            }
-
+        int[] count = new int[10];
+        for (int i=0; i<A.length; ++i) {
+            ++count[A[i]];
         }
 
-        if (thisIndex >= A.length) {
-            return;
+        for (int i=1; i<count.length; ++i) {
+            count[i] += count[i-1];
         }
 
-        // with
-        findMin(thisIndex+1,
-                prevWith+A[thisIndex], prevWithSize+1,
-                prevWithout, prevWithoutSize);
+        Integer[] sorted = new Integer[A.length];
+        for (int i=0; i<A.length; ++i) {
+            sorted[count[A[i]] - 1] = A[i];
+            --count[A[i]];
+        }
 
-        findMin(thisIndex+1,
-                prevWithout+A[thisIndex], prevWithoutSize+1,
-                prevWith, prevWithSize);
-
-
-
+        System.out.print(Arrays.deepToString(sorted));
     }
-
 }
