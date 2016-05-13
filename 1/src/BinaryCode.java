2ee6df29f7f9ab7c18fd912feda7d68ec44817bb
shanxS <shanx.shashank@gmail.com>
2016-05-13 18:51:46 +0530

    pancake sorting

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,19 +1,53 @@
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class BinaryCode {
 
-    private String[] A = {"1", "34", "3", "98", "9", "76", "45", "4"};
+    private Integer[] A = {23, 10, 20, 11, 12, 6, 7};
 
     public void run() {
-        List<String> list = Arrays.asList(A);
 
-        Collections.sort(list, (x,y) -> {
-            return (x+y).compareTo(y+x);
-        });
+        int counter = A.length-1;
+        while (counter > 0) {
+            int maxIndex = findMaxIndex(0, counter);
+            flip(maxIndex);
+            flip(counter);
 
-        System.out.print(list);
+            --counter;
+        }
+
+        System.out.print(Arrays.deepToString(A));
+    }
+
+    private int findMaxIndex(int start, int end) {
+        int max = A[start];
+        int index = start;
+
+        for (int i=start; i<=end; ++i) {
+            if (A[i] > max) {
+                max = A[i];
+                index = i;
+            }
+        }
+
+
+        return index;
+    }
+
+    private void flip (int i) {
+        int rev = i;
+        int fwd = 0;
+
+        while (rev > fwd) {
+            swap (rev, fwd);
+            --rev;
+            ++fwd;
+        }
+    }
+
+    private void swap(int rev, int fwd) {
+        int tmp = A[rev];
+        A[rev] = A[fwd];
+        A[fwd] = tmp;
     }
 
 }
