f65539bd4c3e662c6bfcad7f3918729a860636f1
shanxS <shanx.shashank@gmail.com>
2016-05-13 18:28:00 +0530

    Arrange given numbers to form the biggest number

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,63 +1,19 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class BinaryCode {
 
-    private int[] A = {170, 45, 75, 90, 802, 24, 2, 66};
+    private String[] A = {"1", "34", "3", "98", "9", "76", "45", "4"};
 
     public void run() {
-        Integer passCount = getPassCount();
+        List<String> list = Arrays.asList(A);
 
-        Integer thisPass = 0;
-        while (thisPass < passCount) {
-            A = sortFor(thisPass);
-            ++thisPass;
-        }
+        Collections.sort(list, (x,y) -> {
+            return (x+y).compareTo(y+x);
+        });
 
-        for (int ele : A) {
-            System.out.print(ele + " ");
-        }
+        System.out.print(list);
     }
-
-    private int[] sortFor(Integer thisPass) {
-        int[] count = new int[10];
-        for (int i=0; i<A.length; ++i) {
-            ++count[getNumberFor(A[i], thisPass)];
-        }
-
-        for (int i=1; i<count.length; ++i) {
-            count[i] += count[i-1];
-        }
-
-        int[] sort = new int[A.length];
-        for (int i=A.length-1; i>=0; --i) {
-            sort[count[getNumberFor(A[i], thisPass)] - 1] = A[i];
-            --count[getNumberFor(A[i], thisPass)];
-        }
-
-        return sort;
-    }
-
-    private int getNumberFor(int num, int pass) {
-        while (pass > 0) {
-            num /= 10;
-            --pass;
-        }
-
-        return num%10;
-    }
-
-    private Integer getPassCount() {
-        Integer max = Integer.MIN_VALUE;
-        for (Integer ele : A) {
-            max = Math.max(max, ele);
-        }
-
-        int pass = 0;
-        while (max > 0) {
-            max /= 10;
-            ++pass;
-        }
-
-        return pass;
-    }
-
 
 }
