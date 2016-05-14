f14f6089d213bcc38a2d84db68f442cea9f31d3a
shanxS <shanx.shashank@gmail.com>
2016-05-14 19:05:42 +0530

    converting bt to bst - high time complexity algo

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class BinaryCode {
 
     private Node head;
@@ -9,11 +6,13 @@ public class BinaryCode {
 
         init();
         print(head);
-        System.out.print("----------");
-        Convertor c = new Convertor();
-        c.convert(head);
-        print(head);
+        System.out.print("--------");
 
+        Convertor c = new Convertor();
+        c.convertToBST(head);
+        c.convertToBST(head);
+
+        print(head);
 
     }
 
@@ -34,6 +33,7 @@ public class BinaryCode {
 
         print(node.getLeft());
         print(node.getRight());
+
     }
 
     private void init() {
@@ -41,80 +41,93 @@ public class BinaryCode {
         head.setLeft(new Node(2));
         head.setRight(new Node(7));
 
-        head.getLeft().setLeft(new Node(8));
         head.getLeft().setRight(new Node(4));
+        head.getLeft().setLeft(new Node(8));
 
     }
 }
 
 class Convertor {
-    private List<Integer> numbers;
 
-    public void convert(Node head) {
-        numbers = new ArrayList<>();
+    private Node headHolder;
 
-        traverse(head);
-        sort(0, numbers.size()-1);
-        applyNumbers(head, 0);
-
-    }
-
-    private int applyNumbers(Node node, int pos) {
-        if (node == null) {
-            return pos;
-        }
-
-        pos = applyNumbers(node.getLeft(), pos);
-
-        node.setValue(numbers.get(pos));
-        ++pos;
-
-        return applyNumbers(node.getRight(), pos);
-    }
-
-    private void sort(int start, int end) {
-        if (start >= end) {
-            return;
-        }
-
-        Integer pivot = qSort(start, end);
-        sort(start, pivot-1);
-        sort(pivot+1, end);
-    }
-
-    private Integer qSort(int start, int end) {
-        Integer marker = start-1;
-        Integer pivotValue = numbers.get(end);
-
-        for (Integer i=start; i<end; ++i) {
-            if (numbers.get(i) < pivotValue) {
-                ++marker;
-                swap(i, marker);
-            }
-        }
-
-        swap(marker+1, end);
-        return marker+1;
-    }
-
-    private void swap(int from, int to) {
-        Integer tmp = numbers.get(from);
-        numbers.set(from, numbers.get(to));
-        numbers.set(to, tmp);
-    }
-
-    private void traverse(Node node) {
+    public void print(Node node) {
         if (node == null) {
             return;
         }
 
-        numbers.add(node.getValue());
-        traverse(node.getLeft());
-        traverse(node.getRight());
+        System.out.print(node.getValue() + " - ");
+        if (node.getLeft() != null) {
+            System.out.print(node.getLeft().getValue());
+        }
+        System.out.print(", ");
+        if (node.getRight() != null) {
+            System.out.print(node.getRight().getValue());
+        }
+        System.out.println();
+
+        print(node.getLeft());
+        print(node.getRight());
     }
+
+
+    public void convertToBST(Node head) {
+
+        headHolder = head;
+
+        checkAndCorrect (null, head, null);
+
+
+    }
+
+    private void checkAndCorrect(Node small, Node node, Node big) {
+
+        if (node == null) {
+            return;
+        }
+
+        checkAndCorrect(small, node.getLeft(), node);
+        checkAndCorrect(node, node.getRight(), big);
+
+        if (small != null && big != null) {
+
+            int min = Math.min(node.getValue(), Math.min(small.getValue(), big.getValue()));
+            int max = Math.max(node.getValue(), Math.max(small.getValue(), big.getValue()));
+            int mid = (node.getValue() + big.getValue() + small.getValue())
+                    - (min + max);
+
+            node.setValue(mid);
+            big.setValue(max);
+            small.setValue(min);
+
+        } else if (small != null) {
+
+            int min = Math.min(node.getValue(), small.getValue());
+            int max = Math.max(node.getValue(), small.getValue());
+
+            small.setValue(min);
+            node.setValue(max);
+
+        } else if (big != null) {
+
+            int min = Math.min(node.getValue(), big.getValue());
+            int max = Math.max(node.getValue(), big.getValue());
+
+            node.setValue(min);
+            big.setValue(max);
+
+        } else {
+
+            System.out.print("soemthing fucnked for " + node.getValue());
+
+        }
+
+
+    }
+
 }
 
-class Node{
+class Node {
     private Node left, right;
     private Integer value;
 
