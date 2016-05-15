f7a23c7a957126dcc4643a84c5b33a316d7e2e82
shanxS <shanx.shashank@gmail.com>
2016-05-15 17:24:22 +0530

    edit distance

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,40 +1,36 @@
 public class BinaryCode {
 
-    private char[] A1 = {'A','G', 'G', 'T', 'A', 'B'};
-    private char[] A2 = {'G', 'X', 'T', 'X', 'A', 'Y', 'B'};
+    private String s1 = "sunday";
+    private String s2 = "saturday";
     private int counter = 0;
 
-    private Integer[][] book = new Integer[A1.length][A2.length];
+    private Integer[][] book = new Integer[s1.length()][s2.length()];
 
     public void run() {
 
-        System.out.println(findFor(A1.length-1, A2.length-1));
+        System.out.println(editDistance(s1.length()-1, s2.length()-1));
         System.out.println(counter);
 
     }
 
-    private int findFor(int len1, int len2) {
-
-
+    private int editDistance(int len1, int len2) {
         if (len1 < 0 || len2 < 0) {
-            return 0;
-        } else if (book[len1][len2] != null) {
-            return book[len1][len2];
-        } else {
-
-            int result;
-
-            ++counter;
-
-            if (A1[len1] == A2[len2]) {
-                result = 1 + findFor(len1 - 1, len2 - 1);
-            } else {
-                result = Math.max(findFor(len1 - 1, len2), findFor(len1, len2 - 1));
-            }
-
-            book[len1][len2] = result;
-            return result;
+            return Math.max(len1, len2) + 1;
         }
+
+        if (book[len1][len2] != null) {
+            return book[len1][len2];
+        }
+
+        ++counter;
+
+        if (s1.charAt(len1) == s2.charAt(len2)) {
+            book[len1][len2] =  editDistance(len1-1, len2-1);
+        } else {
+            book[len1][len2] = 1 + Math.min(editDistance(len1, len2-1), Math.min(editDistance(len1-1, len2), editDistance(len1-1, len2-1)));
+        }
+
+        return book[len1][len2];
     }
 
 }
