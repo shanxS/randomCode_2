940cddc8c5f858c32cd93f64523290ad330c89a8
shanxS <shanx.shashank@gmail.com>
2016-05-15 16:44:42 +0530

    Longest Common Subsequence - using recursion and optimal sub structre

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -2,24 +2,38 @@ public class BinaryCode {
 
     private char[] A1 = {'A','G', 'G', 'T', 'A', 'B'};
     private char[] A2 = {'G', 'X', 'T', 'X', 'A', 'Y', 'B'};
+    private int counter = 0;
+
+    private Integer[][] book = new Integer[A1.length][A2.length];
 
     public void run() {
 
-        System.out.print(findFor(A1.length-1, A2.length-1));
+        System.out.println(findFor(A1.length-1, A2.length-1));
+        System.out.println(counter);
 
     }
 
     private int findFor(int len1, int len2) {
 
+
         if (len1 < 0 || len2 < 0) {
             return 0;
-        }
-
-
-        if (A1[len1] ==  A2[len2]) {
-            return 1 + findFor(len1-1, len2-1);
+        } else if (book[len1][len2] != null) {
+            return book[len1][len2];
         } else {
-            return Math.max(findFor(len1-1, len2), findFor(len1, len2-1));
+
+            int result;
+
+            ++counter;
+
+            if (A1[len1] == A2[len2]) {
+                result = 1 + findFor(len1 - 1, len2 - 1);
+            } else {
+                result = Math.max(findFor(len1 - 1, len2), findFor(len1, len2 - 1));
+            }
+
+            book[len1][len2] = result;
+            return result;
         }
     }
 
