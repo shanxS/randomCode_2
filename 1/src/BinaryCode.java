030c793b0a5265b39ac5a413bbd7813fc2d2de0f
shanxS <shanx.shashank@gmail.com>
2016-05-16 19:56:23 +0530

    Matrix Chain Multiplication - attempt 1

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,35 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class BinaryCode {
 
-    private int[] S = {2, 5, 3, 6};
-    private int counter = 0;
-    private final int N = 10;
-    private Integer[][] book = new Integer[S.length][N+1];
 
     public void run() {
 
-        System.out.println((findCount(0, N-1) + findCount(1, N)));
-        System.out.println(counter);
+        List<Integer> A = Arrays.asList(new Integer[] {10, 20, 30});
+
+        System.out.print(findMin(A));
 
     }
 
-    private int findCount(int index, int target) {
+    private int findMin (List<Integer> A) {
 
-        if (target == 0) {
-            return 1;
-        } else if (index >= S.length || target < 0) {
+        if (A.size() < 3) {
             return 0;
         }
 
-        if (book[index][target] != null) {
-            return book[index][target];
+        int minCost = Integer.MAX_VALUE;
+        for (int thisIndex=0; thisIndex<A.size(); ++thisIndex) {
+
+            if (A.size()-thisIndex > 3) {
+
+                List<Integer> listWith = new ArrayList<>(A);
+                listWith.remove((int)(thisIndex+1));
+                int sumWith = A.get(thisIndex) * A.get(thisIndex+1) * A.get(thisIndex+2)
+                        + findMin(listWith);
+
+
+                List<Integer> listWithout = new ArrayList<>(A);
+                listWithout.remove((int)(thisIndex+2));
+                int sumWithout = A.get(thisIndex+1) * A.get(thisIndex+2) * A.get(thisIndex+3)
+                        + findMin(listWithout);
+
+
+                minCost = Math.min(minCost, Math.min(sumWith, sumWithout));
+            } else if (A.size()-thisIndex == 3) {
+
+                List<Integer> list  = new ArrayList<>(A);
+                list.remove((int)(thisIndex+1));
+                int thisSum = A.get(thisIndex) * A.get(thisIndex+1) * A.get(thisIndex+2)
+                        + findMin(list);
+
+                minCost = Math.min(minCost, thisSum);
+            } else {
+                break;
+            }
         }
 
-        ++counter;
-
-        book[index][target] =  findCount(index, target-S[index])
-                + findCount(index+1, target);
-
-        return book[index][target];
+        return minCost;
     }
 
 }
