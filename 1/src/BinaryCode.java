bdaabef3ed85534ad31fc824d43a05734ece056d
shanxS <shanx.shashank@gmail.com>
2016-05-17 20:53:29 +0530

    counting sort practice

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,56 +1,31 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class BinaryCode {
 
+    private Integer[] A = {4,2,3,1,6};
 
     public void run() {
 
-        List<Integer> A = Arrays.asList(new Integer[] {10, 20, 30});
+        int[] count = new int[10];
+        for (int i=0; i<A.length; ++i) {
+            ++count[A[i]];
+        }
 
-        System.out.print(findMin(A));
+        for (int i=1; i<count.length; ++i) {
+            count[i] += count[i-1];
+        }
+
+        int[] sort = new int[A.length];
+        for (int i=0; i<A.length; ++i) {
+
+            sort[count[A[i]] - 1] = A[i];
+            --count[A[i]];
+
+        }
+
+        for (int i=0; i<sort.length; ++i) {
+            System.out.print(sort[i] + " ");
+        }
 
     }
 
-    private int findMin (List<Integer> A) {
-
-        if (A.size() < 3) {
-            return 0;
-        }
-
-        int minCost = Integer.MAX_VALUE;
-        for (int thisIndex=0; thisIndex<A.size(); ++thisIndex) {
-
-            if (A.size()-thisIndex > 3) {
-
-                List<Integer> listWith = new ArrayList<>(A);
-                listWith.remove((int)(thisIndex+1));
-                int sumWith = A.get(thisIndex) * A.get(thisIndex+1) * A.get(thisIndex+2)
-                        + findMin(listWith);
-
-
-                List<Integer> listWithout = new ArrayList<>(A);
-                listWithout.remove((int)(thisIndex+2));
-                int sumWithout = A.get(thisIndex+1) * A.get(thisIndex+2) * A.get(thisIndex+3)
-                        + findMin(listWithout);
-
-
-                minCost = Math.min(minCost, Math.min(sumWith, sumWithout));
-            } else if (A.size()-thisIndex == 3) {
-
-                List<Integer> list  = new ArrayList<>(A);
-                list.remove((int)(thisIndex+1));
-                int thisSum = A.get(thisIndex) * A.get(thisIndex+1) * A.get(thisIndex+2)
-                        + findMin(list);
-
-                minCost = Math.min(minCost, thisSum);
-            } else {
-                break;
-            }
-        }
-
-        return minCost;
-    }
 
 }
