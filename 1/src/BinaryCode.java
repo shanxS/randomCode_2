06006846d3f63f57264eec459affc78eaa7c6501
shanxS <shanx.shashank@gmail.com>
2016-05-17 22:53:14 +0530

    Matrix Chain Multiplication improving for space complexity

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,31 +1,31 @@
 public class BinaryCode {
 
-    private Integer[] A = {4,2,3,1,6};
+    private Integer[] A = {10,20};
 
     public void run() {
 
-        int[] count = new int[10];
-        for (int i=0; i<A.length; ++i) {
-            ++count[A[i]];
-        }
-
-        for (int i=1; i<count.length; ++i) {
-            count[i] += count[i-1];
-        }
-
-        int[] sort = new int[A.length];
-        for (int i=0; i<A.length; ++i) {
-
-            sort[count[A[i]] - 1] = A[i];
-            --count[A[i]];
-
-        }
-
-        for (int i=0; i<sort.length; ++i) {
-            System.out.print(sort[i] + " ");
-        }
+        System.out.print(getMin(1, A.length-1));
 
     }
 
+    private int getMin(int start, int end) {
+
+        if (start == end) {
+            return 0;
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i=start; i<end; ++i) {
+            min = Math.min(min, (
+                    getMin(start, i) +
+                    getMin(i+1, end) +
+                            (A[start-1] * A[i] * A[end])
+
+                    ));
+        }
+
+
+        return min;
+    }
 
 }
