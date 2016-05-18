e8c1f50b4eeac6b50458ad0bbef4d41929ce476c
shanxS <shanx.shashank@gmail.com>
2016-05-18 19:32:11 +0530

    Matrix Chain Multiplication - optimising

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,31 +1,35 @@
 public class BinaryCode {
 
-    private Integer[] A = {10,20};
+    private Integer[] A = {40, 20, 30, 10, 30};
+    private Integer[][] book = new Integer[A.length][A.length];
+
+    int counter = 0;
 
     public void run() {
-
-        System.out.print(getMin(1, A.length-1));
+        System.out.print(findMin(1, A.length-1) + " " + counter);
 
     }
 
-    private int getMin(int start, int end) {
-
+    private int findMin(int start, int end) {
         if (start == end) {
             return 0;
         }
 
-        int min = Integer.MAX_VALUE;
-        for (int i=start; i<end; ++i) {
-            min = Math.min(min, (
-                    getMin(start, i) +
-                    getMin(i+1, end) +
-                            (A[start-1] * A[i] * A[end])
-
-                    ));
+        if (book[start][end] != null) {
+            return book[start][end];
         }
 
+        ++counter;
 
-        return min;
+        int min = Integer.MAX_VALUE;
+        for (int i=start; i<end; ++i) {
+            min = Math.min(min,
+                    findMin(start,i)
+                            + findMin(i+1, end)
+                            + (A[start-1] * A[i] * A[end]));
+        }
+
+        book[start][end] = min;
+        return book[start][end];
     }
-
 }
