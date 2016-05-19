8f931849db4321312e2645eebef0e14286ade95e
shanxS <shanx.shashank@gmail.com>
2016-05-19 20:42:26 +0530

    0-1 Knapsack Problem

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,45 +1,27 @@
 public class BinaryCode {
 
-    private Integer[][] book;
-
-
+    private int val[] = {60, 100, 120};
+    private int wt[] = {10, 20, 30};
+    private int  W = 50;
 
     public void run() {
 
-        int n=6, k=4;
-        book = new Integer[n+1][k+1];
-
-        System.out.println(find(n,k));
-
-        for (int r=0; r<book.length; ++r) {
-            System.out.println();
-            for (int c=0; c<book[0].length; ++c) {
-                System.out.print(book[r][c] + " ");
-            }
-        }
-
-
+        System.out.print(Math.max(findMaxValue(1, W-wt[0], val[0]),
+                findMaxValue(1, W, 0)));
 
     }
 
-    private int find(int n, int k) {
-        if (n < 1 || k < 1 || k > n) {
+    private int findMaxValue(int thisIndex, int w, int valueSoFar) {
+        if (w == 0) {
+            return valueSoFar;
+        } else if (w < 0) {
             return -1;
+        } else if (thisIndex >= wt.length) {
+            return valueSoFar;
         }
 
-        if (book[n][k] != null) {
-            return book[n][k];
-        }
-
-        if (k == n) {
-            book[n][k] = 1;
-        } else if (k==1) {
-            book[n][k] = n;
-        } else {
-            book[n][k] = find(n - 1, k) + find(n - 1, k - 1);
-        }
-
-        return book[n][k];
+        int inclusive = (w-wt[thisIndex] >= 0) ? findMaxValue(thisIndex+1, w-wt[thisIndex], val[thisIndex] + valueSoFar) : valueSoFar;
+        return Math.max(inclusive,
+                findMaxValue(thisIndex + 1, w, valueSoFar));
     }
-
 }
