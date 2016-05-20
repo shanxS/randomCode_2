4e853f5f47174a1a76a55f105c602137ea4812a9
shanxS <shanx.shashank@gmail.com>
2016-05-20 22:29:04 +0530

    given eggs and floors - find min floor from which egg doesnt break - in min count - memoised

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,29 +1,39 @@
 public class BinaryCode {
 
+    private Integer[][] book;
+
     public void run() {
 
-        int eggCount=2, floor=10;
+        int eggCount=20, floor=4000;
+
+        book = new Integer[floor+1][eggCount+1];
 
         System.out.print(findMin(eggCount, floor));
     }
 
     private int findMin(int eggCount, int floor) {
 
+        if (book[floor][eggCount] != null) {
+            return book[floor][eggCount];
+        }
+
+
         if (eggCount == 1) {
-            return floor;
+            book[floor][eggCount] = floor;
+        } else if (floor == 0 || floor == 1) {
+            book[floor][eggCount] = floor;
+        } else {
+
+            int min = Integer.MAX_VALUE;
+            for (int thisFloor = 1; thisFloor <= floor; ++thisFloor) {
+                min = Math.min(min,
+                        Math.max(findMin(eggCount - 1, thisFloor - 1), findMin(eggCount, floor - thisFloor)));
+            }
+
+            book[floor][eggCount] = min + 1;
         }
 
-        if (floor == 0 || floor == 1) {
-            return floor;
-        }
-
-        int min = Integer.MAX_VALUE;
-        for (int thisFloor=1; thisFloor<=floor; ++thisFloor) {
-            min = Math.min(min,
-                    Math.max(findMin(eggCount-1, thisFloor-1), findMin(eggCount, floor-thisFloor)));
-        }
-
-        return min+1;
+        return book[floor][eggCount];
 
     }
 
