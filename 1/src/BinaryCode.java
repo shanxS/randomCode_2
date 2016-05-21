a9ec22fe851f016b8e12137ecc2c0c134d083f43
Kumar <shashaku@amazon.com>
2016-05-21 17:58:58 +0530

    longest palindromic subseq - better approach

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,44 +1,37 @@
 public class BinaryCode {
 
-    private String s = "GEEKSFORGEEKS";
+    private String s = "BBABCBCAB";
 
     public void run() {
 
-        System.out.print(Math.max(findMaxPalinLength(1, ""+s.charAt(0)),
-                findMaxPalinLength(1, "")));
-    }
-
-    private int findMaxPalinLength(int thisIndex, String strSoFar) {
-        if (thisIndex >= s.length()) {
-            if (isPalindrome(strSoFar)) {
-                return strSoFar.length();
-            } else {
-                return -1;
-            }
+        int max = Integer.MIN_VALUE;
+        for (int startPoint = 0; startPoint<s.length(); ++startPoint) {
+            max = Math.max(max, Math.max(
+                    findMaxPalinLength(startPoint,startPoint),
+                    findMaxPalinLength(startPoint-1, startPoint)));
         }
 
-        int thisLen = (isPalindrome(strSoFar)) ? strSoFar.length() : -1;
-        return Math.max(thisLen,
-                Math.max(findMaxPalinLength(thisIndex+1, strSoFar+s.charAt(thisIndex)),
-                findMaxPalinLength(thisIndex+1, strSoFar)));
+        System.out.print(max);
     }
 
-
-    private boolean isPalindrome (String s) {
-        int fwd = 0;
-        int rev = s.length()-1;
-
-        while (rev > fwd) {
-            if (s.charAt(rev) != s.charAt(fwd)) {
-                return false;
-            }
-
-            ++fwd;
-            --rev;
+    private int findMaxPalinLength(int startIndex, int endIndex) {
+        if (startIndex < 0 || endIndex >= s.length()) {
+            return 0;
+        } else if (startIndex == endIndex) {
+            return 1 + findMaxPalinLength(startIndex-1, endIndex+1);
         }
 
-        return true;
-    }
 
+        if (s.charAt(startIndex) == s.charAt(endIndex)) {
+
+            return 2 + findMaxPalinLength(startIndex-1, endIndex+1);
+
+        } else {
+
+            return Math.max(findMaxPalinLength(startIndex, endIndex+1),
+                    findMaxPalinLength(startIndex-1, endIndex));
+
+        }
+    }
 
 }
