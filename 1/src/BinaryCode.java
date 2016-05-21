8d0e5b65bcbb9df56214db7f2427c41bfd2699a0
Kumar <shashaku@amazon.com>
2016-05-21 18:14:14 +0530

    Longest Palindromic Subsequence - with memoisation

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,27 +1,38 @@
 public class BinaryCode {
 
     private String s = "BBABCBCAB";
+    private int count = 0;
+    private Integer[][] book = new Integer[s.length()][s.length()];
 
     public void run() {
 
-        System.out.print(findMaxPalnLength(0, s.length()-1));
+        System.out.print(findMaxPalnLength(0, s.length()-1) + " " + count);
 
     }
 
     private int findMaxPalnLength(int start, int end) {
-        if (start == end) {
-            return 1;
-        } else if (start > end) {
+        if (book[start][end] != null) {
+            return book[start][end];
+        }
+
+        if (start > end) {
             return 0;
+        }
+
+        ++count;
+        if (start == end) {
+            book[start][end] = 1;
         } else {
 
             if (s.charAt(start) == s.charAt(end)) {
-                return 2 + findMaxPalnLength(start + 1, end - 1);
+                book[start][end] = 2 + findMaxPalnLength(start + 1, end - 1);
             } else {
-                return Math.max(findMaxPalnLength(start + 1, end),
+                book[start][end] = Math.max(findMaxPalnLength(start + 1, end),
                         findMaxPalnLength(start, end - 1));
             }
         }
+
+        return book[start][end];
     }
 
 
