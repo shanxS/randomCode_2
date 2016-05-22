c09739250c1a1e6da31821127c4689bb515ac1b4
shanxS <shanx.shashank@gmail.com>
2016-05-22 16:21:41 +0530

    Cutting a Rod - better solution

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,43 +1,39 @@
 public class BinaryCode {
 
-    private Integer[] A = {1,   5,   8,   9,  10,  17,  17,  20};//{1,   5,   8,   9,  10,  17,  17,  20};
+    private Integer[] A = {3,   5,   8,   9,  10,  17,  17,  20};//{1,   5,   8,   9,  10,  17,  17,  20};
 
     public void run() {
 
         int thisIndex = 0;
 
         System.out.print(Math.max(
-                findMax(thisIndex+1, A[thisIndex], A.length-(thisIndex+1)),
+                    A[thisIndex] + findMax(thisIndex+1, A.length-(thisIndex+1)),
+                    Math.max(
+                            findMax(thisIndex+1, A.length),
+                            A[thisIndex] + findMax(thisIndex, A.length-(thisIndex+1))
+                    )
 
-                Math.max(
-                        findMax(thisIndex+1, 0, A.length),
-                        findMax(thisIndex, A[thisIndex], A.length-(thisIndex+1))
                 )
-        ));
+        );
 
     }
 
-    private int findMax(int thisIndex, Integer valueSoFar, int sizeAvailable) {
+    private int findMax(int thisIndex, int sizeAvailable) {
         if (thisIndex >= A.length) {
-            return valueSoFar;
-        } else if (sizeAvailable == 0) {
-            return valueSoFar;
+            return 0;
+        } else if (thisIndex+1 > sizeAvailable) {
+            return 0;
         }
 
-        if ((thisIndex+1) <= sizeAvailable) {
+        return Math.max(
+                A[thisIndex] + findMax(thisIndex+1, sizeAvailable-(thisIndex+1)),
+                Math.max(
+                        findMax(thisIndex+1, sizeAvailable),
+                        A[thisIndex] + findMax(thisIndex, sizeAvailable-(thisIndex+1))
+                )
 
-            return Math.max(
-                    findMax(thisIndex+1, valueSoFar+A[thisIndex], sizeAvailable-(thisIndex+1)),
+        );
 
-                    Math.max(
-                        findMax(thisIndex+1, valueSoFar, sizeAvailable),
-                        findMax(thisIndex, valueSoFar+A[thisIndex], sizeAvailable-(thisIndex+1))
-                    )
-            );
-
-        } else {
-            return valueSoFar;
-        }
     }
 
 }
