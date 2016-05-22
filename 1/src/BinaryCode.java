5f4ca2a9e25fb7d90c5259c60512c4e67745e6bd
shanxS <shanx.shashank@gmail.com>
2016-05-22 16:07:29 +0530

    Cutting a Rod - basic solution

--- 1/src/BinaryCode.java
+++ 1/src/BinaryCode.java
@@ -1,22 +1,43 @@
 public class BinaryCode {
 
-    private Integer[] A = {1,   5,   8,   9,  10,  17,  17,  20};
+    private Integer[] A = {1,   5,   8,   9,  10,  17,  17,  20};//{1,   5,   8,   9,  10,  17,  17,  20};
 
     public void run() {
 
+        int thisIndex = 0;
 
-        System.out.print(Math.max(findMaxValue (1, 1),
-                findMaxValue (1, 0)));
+        System.out.print(Math.max(
+                findMax(thisIndex+1, A[thisIndex], A.length-(thisIndex+1)),
+
+                Math.max(
+                        findMax(thisIndex+1, 0, A.length),
+                        findMax(thisIndex, A[thisIndex], A.length-(thisIndex+1))
+                )
+        ));
 
     }
 
-    private int findMaxValue(int thisIndex, int valueSoFar) {
-        if (A.length - thisIndex <= 0) {
+    private int findMax(int thisIndex, Integer valueSoFar, int sizeAvailable) {
+        if (thisIndex >= A.length) {
+            return valueSoFar;
+        } else if (sizeAvailable == 0) {
             return valueSoFar;
         }
 
-        return Math.max(findMaxValue(thisIndex+1, valueSoFar+A[thisIndex]),
-                findMaxValue(thisIndex+1, valueSoFar));
+        if ((thisIndex+1) <= sizeAvailable) {
+
+            return Math.max(
+                    findMax(thisIndex+1, valueSoFar+A[thisIndex], sizeAvailable-(thisIndex+1)),
+
+                    Math.max(
+                        findMax(thisIndex+1, valueSoFar, sizeAvailable),
+                        findMax(thisIndex, valueSoFar+A[thisIndex], sizeAvailable-(thisIndex+1))
+                    )
+            );
+
+        } else {
+            return valueSoFar;
+        }
     }
 
 }
