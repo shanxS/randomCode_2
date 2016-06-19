2a31f9c526a1aee7195e2ecb2baadd1c898183a1
shanxS <shanx.shashank@gmail.com>
2016-06-19 17:26:24 +0530

    level order traversal BST

--- 1/src/Algo.java
+++ 1/src/Algo.java
@@ -1,84 +1,81 @@
 import lombok.*;
+import java.util.*;
 
 public class Algo {
 
-    private int[] A = {100, 50, 110, 25, 60};
+    private int[] A = {100, 50, 210, 25, 60, 110, 250};
 
     public void run() {
+
+
         BST tree = new BST();
         for (Integer i : A) {
             tree.insert(i);
         }
 
         tree.print();
-
-        System.out.println("inorder ");
-        tree.printInorderTraversal();
-        System.out.println("preorder ");
-        tree.printPreOrderTraversal();
-        System.out.println("postorder ");
-        tree.printPostOrderTraversal();
+        System.out.println ("breadthwise ");
+        tree.breadthWisePrint();
 
     }
 }
 
 
-class BST{
-
+class BST {
     private Node head;
-
-    public void printInorderTraversal() {
-        printInorderTraversal(head);
-    }
-
-    private void printInorderTraversal(Node node) {
-        if (node == null) {
-            return;
-        }
-
-        printInorderTraversal(node.getLeft());
-        System.out.println(node.getValue());
-        printInorderTraversal(node.getRight());
-    }
-
-
-    public void printPostOrderTraversal() {
-        printPostOrderTraversal(head);
-    }
-
-    private void printPostOrderTraversal(Node node) {
-        if (node == null) {
-            return;
-        }
-
-        printPostOrderTraversal(node.getLeft());
-        printPostOrderTraversal(node.getRight());
-        System.out.println(node.getValue());
-    }
-
-    public void printPreOrderTraversal() {
-        printPreOrderTraversal(head);
-    }
-
-    private void printPreOrderTraversal(Node node) {
-        if (node == null) {
-            return;
-        }
-
-        System.out.println(node.getValue());
-        printPreOrderTraversal(node.getLeft());
-        printPreOrderTraversal(node.getRight());
-    }
-
-
 
     public void insert(int val) {
         if (head == null) {
             head = new Node (val);
         } else {
-            insert(head, val);
+            insert(head,val);
         }
     }
+
+
+    public void breadthWisePrint() {
+        breadthWisePrint(head);
+    }
+
+    private void breadthWisePrint(Node node) {
+
+        Stack<Node> stack1 = new Stack<>();
+        Stack<Node> stack2 = new Stack<>();
+
+        stack1.push(node);
+        while (stack1.size() > 0 || stack2.size() > 0) {
+
+            while (stack1.size() > 0) {
+                Node thisNode = stack1.pop();
+                System.out.print(thisNode.getValue() + " ");
+
+                if (thisNode.getLeft() != null) {
+                    stack2.push(thisNode.getLeft());
+                }
+                if (thisNode.getRight() != null) {
+                    stack2.push(thisNode.getRight());
+                }
+            }
+
+            System.out.println();
+
+            while (stack2.size() > 0) {
+                Node thisNode = stack2.pop();
+                System.out.print(thisNode.getValue() + " ");
+
+                if (thisNode.getLeft() != null) {
+                    stack1.push(thisNode.getLeft());
+                }
+                if (thisNode.getRight() != null) {
+                    stack1.push(thisNode.getRight());
+                }
+            }
+            System.out.println();
+
+        }
+
+    }
+
 
     private void insert(Node node, int val) {
         if (node.getValue() < val) {
@@ -89,12 +86,12 @@ class BST{
             }
         } else if (node.getValue() > val) {
             if (node.getLeft() == null) {
-                node.setLeft(new Node (val));
+                node.setLeft(new Node(val));
             } else {
                 insert(node.getLeft(), val);
             }
         } else {
-            System.out.print("duplicate value found for " + val);
+            System.out.print("same value " + val);
         }
     }
 
@@ -102,14 +99,13 @@ class BST{
         print(head);
     }
 
-
     private void print(Node node) {
         if (node == null) {
             return;
         }
 
-        System.out.print(node.getValue() + " - ");
-        if (node.getLeft() != null) {
+        System.out.print(node.getValue()+ " - ");
+        if(node.getLeft() != null) {
             System.out.print(node.getLeft().getValue());
         }
         System.out.print(", ");
@@ -118,24 +114,21 @@ class BST{
         }
         System.out.println();
 
-
         print(node.getLeft());
         print(node.getRight());
+
     }
-
-
 }
 
 
-class Node{
 
-    @Getter @Setter
+class Node {
+    @Setter @Getter
     private Node left, right;
-
     @Getter
     private Integer value;
 
-    public Node (int v) {
-        this.value = v;
+    public Node (int val) {
+        this.value = val;
     }
 }
