b637d02c3d29bb32ab9a836933a14d40ca92eefb
shanxS <shanx.shashank@gmail.com>
2016-06-24 21:13:58 +0530

    quick sort

--- 1/src/Main.java
+++ 1/src/Main.java
@@ -1,5 +1,3 @@
-import lombok.*;
-
 public class Main {
     public static void main(String[] args) {
         Algo algo = new Algo();
@@ -17,182 +15,41 @@ class Algo {
             530,570,970
     };
 
-
     public void run() {
-
-        BST tree = new BST();
+        sort(0, A.length-1);
         for (Integer i : A) {
-            tree.insert(i);
-        }
-
-        BST.print(tree.getHead());
-
-        System.out.print("------------");
-        NodeDeleter nd = new NodeDeleter();
-        Node head = nd.deleteNode(tree.getHead(), 500);
-        BST.print(head);
-
-    }
-}
-
-class NodeDeleter {
-
-    public Node deleteNode (Node node, int val) {
-        Node head = node;
-        NodeContext dContext = makeDeletionContext(null, node, val);
-        if (dContext == null) {
-            System.out.print("cant find node to detele");
-            return head;
-        }
-
-        Node dNode = dContext.getTarget();
-        NodeContext rContext = null;
-        if (dNode.getLeft() == null && dNode.getRight() == null) {
-            rContext = null;
-        } else if (dNode.getLeft() != null && dNode.getRight() == null) {
-            rContext = new NodeContext(null, dNode.getLeft());
-        } else if (dNode.getRight() != null) {
-            rContext = makeMaxNodeContext(dNode, dNode.getRight());
-            deleteNode(rContext.getParent(), rContext.getTarget().getValue());
-        }
-        dropReplacement(dContext, rContext);
-
-        if (dContext.getParent() == null) {
-            return rContext.getTarget();
-        } else {
-            return head;
-        }
-
-    }
-
-    private void dropReplacement(NodeContext dContext, NodeContext rContext) {
-        Node dParent = dContext.getParent();
-        Node dTarget = dContext.getTarget();
-
-        if (rContext == null) {
-
-            if (dParent != null) {
-                if (dParent.getValue() < dTarget.getValue()) {
-                    dParent.setRight(null);
-                } else {
-                    dParent.setLeft(null);
-                }
-            }
-        } else {
-
-            Node rParent = rContext.getParent();
-            Node rTarget = rContext.getTarget();
-
-            if ( !(dTarget.getLeft() != null && dTarget.getLeft().getValue() == rTarget.getValue())) {
-                rTarget.setLeft(dTarget.getLeft());
-                rTarget.setRight(dTarget.getRight());
-            }
-
-            if (dParent != null) {
-                if (dParent.getValue() < dTarget.getValue()) {
-                    dParent.setRight(rTarget);
-                } else {
-                    dParent.setLeft(rTarget);
-                }
-            }
-
+            System.out.print(i + " ");
         }
     }
 
-    private NodeContext makeMaxNodeContext (Node parent, Node node) {
-        while (node.getRight() != null) {
-            parent = node;
-            node = node.getRight();
-        }
-
-        return new NodeContext(parent, node);
-    }
-
-    private NodeContext makeDeletionContext(Node parent, Node node, int val) {
-        if (node == null) {
-            return null;
-        }
-
-        if (node.getValue() == val) {
-            return new NodeContext(parent, node);
-        } else if (node.getValue() < val) {
-            return makeDeletionContext(node, node.getRight(), val);
-        } else {
-            return makeDeletionContext(node, node.getLeft(), val);
-        }
-    }
-
-    private class NodeContext {
-        @Getter
-        private Node parent, target;
-
-        public NodeContext(Node parent, Node target) {
-            this.parent = parent;
-            this.target = target;
-        }
-    }
-}
-
-class BST {
-    @Getter
-    private Node head;
-
-    public void insert(int val)  {
-        if (head == null) {
-            head = new Node (val);
-        } else {
-            insert(head, val);
-        }
-    }
-
-    private void insert(Node node, int val) {
-        if (node.getValue() < val) {
-            if (node.getRight() == null) {
-                node.setRight(new Node(val));
-            } else {
-                insert(node.getRight(), val);
-            }
-        } else if (node.getValue() > val){
-            if (node.getLeft() == null) {
-                node.setLeft(new Node(val));
-            } else {
-                insert(node.getLeft(), val);
-            }
-        } else {
-            System.out.print("duplicate vlaue " + val);
-        }
-    }
-
-    public static void print(Node node) {
-        if (node== null) {
+    private void sort(int start, int end) {
+        if (start >= end) {
             return;
         }
 
-        System.out.print(node.getValue() + " - ");
-        if (node.getLeft() != null) {
-            System.out.print(node.getLeft().getValue());
-        }
-        System.out.print(", ");
-        if (node.getRight() != null) {
-            System.out.print(node.getRight().getValue());
-        }
-        System.out.println();
-
-        print(node.getLeft());
-        print(node.getRight());
+        int pivotIndex = qsort(start, end);
+        sort(start, pivotIndex-1);
+        sort(pivotIndex+1, end);
     }
 
-}
+    private int qsort(int start, int end) {
+        int marker = start-1;
+        int pivot = A[end];
 
-class Node {
+        for (int i=start; i<end; ++i) {
+            if (A[i] < pivot) {
+                ++marker;
+                swap(marker, i);
+            }
+        }
 
-    @Setter @Getter
-    private Node left, right;
+        swap(marker+1, end);
+        return marker+1;
+    }
 
-    @Setter @Getter
-    private Integer value;
-
-    public Node(int val) {
-        this.value = val;
+    private void swap (int from, int to) {
+        int tmp = A[to];
+        A[to] = A[from];
+        A[from] = tmp;
     }
 }
