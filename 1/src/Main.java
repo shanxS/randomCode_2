77340d3f7b1d2e977d7a72228ba20b3a84040e1b
shanxS <shanx.shashank@gmail.com>
2016-07-03 19:12:14 +0530

    floyd warshl

--- 1/src/Main.java
+++ 1/src/Main.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,48 +10,64 @@ public class Main {
 class Algo {
     private final int I = Integer.MAX_VALUE;
     private int[][] A = {
-            {0,5,2,9,I,I,I},
-            {I,0,3,I,3,2,1},
-            {I,I,0,1,I,I,I},
-            {8,I,I,0,I,I,I},
-            {I,I,I,I,I,I,I},
-            {I,I,I,I,I,I,I},
-            {I,I,I,I,I,I,I}
+          // 0 1 2 3 4 5 6
+            {0,5,6,9,1,I,I}, // 0
+            {I,0,3,I,3,2,1}, // 1
+            {I,I,0,1,I,I,I}, // 2
+            {8,I,I,0,I,I,I}, // 3
+            {I,I,1,I,0,I,I}, // 4
+            {I,I,I,I,I,0,I}, // 5
+            {I,I,I,I,I,I,0}  // 6
 
     };
 
-    private int[][] min;
-    private boolean[] visited;
+    private int nodeCount;
 
 
     public void run() {
 
-        visited = new boolean[A.length];
+        nodeCount = A.length;
 
-        for (int i=0; i<visited.length; ++i) {
-            if (!visited[i]) {
-                bfs(i);
-            }
-        }
-    }
+        for (int src = 0; src < nodeCount; ++src) {
+            for (int target=0; target<nodeCount; ++target) {
+                if (src != target) {
+                    int min = A[src][target];
 
-    private void bfs(int node) {
-        Deque<Integer> que = new ArrayDeque<>();
-        que.addLast(node);
-        visited[node] = true;
+                    for(int i=0; i<nodeCount; ++i) {
+                        if (i!=target && i!= src && A[src][i] != I) {
+                            boolean[] visited = new boolean[nodeCount];
+                            visited[src] = true;
+                            min = Math.min(min, findMinCost(src, target, visited, i, A[src][i]));
+                        }
+                    }
 
-        while(que.size() > 0) {
-
-            int thisNode = que.removeFirst();
-            System.out.print(thisNode + " ");
-            for(int child=0; child<visited.length; ++child) {
-                if (!visited[child] && A[thisNode][child] != I && thisNode != child) {
-                    visited[child] = true;
-                    que.addLast(child);
+                    System.out.println("src " + src + " taget " + target + " minDist " + min);
                 }
             }
         }
+
     }
+
+    private int findMinCost(int src, int target, boolean[] visited, int thisNode, int costSoFar) {
+
+        if (thisNode == target) {
+            return costSoFar;
+        }
+
+        int min = A[thisNode][target];
+        for (int subNode = 0; subNode<nodeCount; ++subNode) {
+            if (!visited[subNode] && A[thisNode][subNode] != I) {
+                boolean[] thisVisited = Arrays.copyOf(visited, visited.length);
+                thisVisited[subNode] = true;
+                min = Math.min(min, findMinCost(src, target, thisVisited, subNode, A[thisNode][subNode]));
+            }
+        }
+
+        return (min == I) ? I : min + costSoFar;
+
+    }
+
+
 }
 
 
